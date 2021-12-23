2f6b4bd87ac17a15e97214966505f54c761fb962
jisungbin <ji@sungb.in>
2021-12-23 22:12:06 +0900

    tlqkf

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,51 +1,58 @@
+import java.io.BufferedReader
 import java.io.BufferedWriter
+import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.Scanner
-
-// 물을 가지고 있으며, 자식 정점이 있다면 자식 정점 중 하나를 골라 물을 1 준다. 자식 정점이 여러 개라면 동일한 확률로 그 중 하나를 고른다.
-// -> 리프 노드일 때만 물을 가지고 있을 수 있음 (자식 노드가 있다면, 자식 노드에게 계속 물을 넘김)
+import kotlin.math.max
 
 private data class Node(val key: Int, val weight: Int)
 
 fun main() {
-    val scanner = Scanner(System.`in`)
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val nodeSize = scanner.nextInt()
+    val (nodeSize, rootNode) = br.readLine()!!.split(" ").map { it.toInt() }
     val nodes = MutableList(nodeSize + 1) { mutableListOf<Node>() }
 
-    var treeWeight = 0
-    var deepNode = 0
+    var maxLeafNodeWeightSum = 0
+    var gigaNode = 0
+    var treeHeight = 0 // 트리 기둥 길이
 
     repeat(nodeSize - 1) {
-        val node = scanner.nextInt()
-        while (true) {
-            val node2 = scanner.nextInt()
-            if (node2 == -1) break
-            val weight = scanner.nextInt()
-            nodes[node].add(Node(key = node2, weight = weight))
+        val (node, node2, weight) = br.readLine()!!.split(" ").map { it.toInt() }
+        nodes[node].add(Node(key = node2, weight = weight))
+        nodes[node2].add(Node(key = node, weight = weight))
+    }
+
+    fun setGigaNodeAndTreeHeigth(node: Int = rootNode, parent: Int = 0, height: Int = 0) {
+        val newNodes = nodes[node].filterNot { it.key == parent }
+        if (newNodes.isEmpty() || newNodes.size >= 2) {
+            gigaNode = node
+            treeHeight = height
+        } else {
+            setGigaNodeAndTreeHeigth(
+                node = newNodes.first().key,
+                parent = node,
+                height = height + newNodes.first().weight
+            )
         }
     }
 
-    fun dfs(node: Int = 1, parent: Int = 0, weight: Int = 0) {
-        if (treeWeight < weight) {
-            treeWeight = weight // 트리 지름 더 큰 걸로 업데이트 (최대 지름으로 갱신)
-            deepNode = node // 지름이 더 깊어지는 노드가, 제일 깊은 노드임
-        }
+    fun dfs(node: Int = gigaNode, parent: Int = 0, weight: Int = 0) {
+        maxLeafNodeWeightSum = max(maxLeafNodeWeightSum, weight)
 
         for (newNode in nodes[node]) {
             if (newNode.key != parent) {
-                dfs(node = newNode.key, parent = node, weight = newNode.weight + weight)
+                dfs(node = newNode.key, parent = node, weight = weight + newNode.weight)
             }
         }
     }
 
-    dfs() // 제일 깊은 노드를 찾음
-    dfs(node = deepNode) // 위에서 찾은 제일 깊은 노드로, 트리의 지름을 측정함
+    setGigaNodeAndTreeHeigth()
+    dfs()
 
-    bw.write("$treeWeight")
+    bw.write("$treeHeight $maxLeafNodeWeightSum")
 
-    scanner.close()
+    br.close()
     bw.flush()
     bw.close()
 }
