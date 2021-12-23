73770fc120ea4fa6ffef151be19a6f666489f2b2
jisungbin <ji@sungb.in>
2021-12-23 18:12:01 +0900

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -64,4 +65,4 @@ class Node {
         this.node2 = node2;
         this.weight = weight;
     }
-}
+}*/
