423270e7252ad63c2ad756d2942c0e69754dfac5
jisungbin <ji@sungb.in>
2021-12-23 17:28:22 +0900

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,23 +5,31 @@ import java.io.OutputStreamWriter
 
 // 물을 가지고 있으며, 자식 정점이 있다면 자식 정점 중 하나를 골라 물을 1 준다. 자식 정점이 여러 개라면 동일한 확률로 그 중 하나를 고른다.
 // -> 리프 노드일 때만 물을 가지고 있을 수 있음 (자식 노드가 있다면, 자식 노드에게 계속 물을 넘김)
+
+private data class Node(var node2: Int = 0, var weight: Int = 0)
+
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val (nodeSize, waterSize) = br.readLine()!!.split(" ").map { it.toInt() }
-    val forEachNodeCount = MutableList(nodeSize + 1) { 0 }
+    val nodeSize = br.readLine()!!.toInt()
+    val nodes = MutableList(nodeSize + 1) { Node() }
+
+    var treeWeight = 0
+    var deepNode = 0
+
     repeat(nodeSize - 1) {
-        val (node, node2) = br.readLine()!!.split(" ").map { it.toInt() }
-        forEachNodeCount[node]++
-        forEachNodeCount[node2]++
+        val (node, node2, weight) = br.readLine()!!.split(" ").map { it.toInt() }
+        nodes[node] = Node(node2 = node2, weight = weight)
+        nodes[node2] = Node(node2 = node, weight = weight)
     }
 
-    var leafNodeCount = 0.toDouble()
-    for (i in 2..nodeSize) {
-        if (forEachNodeCount[i] == 1) leafNodeCount++
+    fun dfs(node: Int, parent: Int, weight: Int) {
+        if (treeWeight < weight) {
+            treeWeight = weight //
+            deepNode = node
+        }
     }
-    bw.write("${waterSize / leafNodeCount}")
 
     br.close()
     bw.flush()
