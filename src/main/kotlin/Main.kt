80a16e2d69074b4c204643394fd105b232c46d55
jisungbin <ji@sungb.in>
2021-12-27 21:08:45 +0900

    난 수학문제가 싫다.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,35 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
-private data class Node(var parent: Int = -1, var key: Int, var left: Int = -1, var right: Int = -1)
-
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val caseCount = br.readLine()!!.toInt()
-    repeat(caseCount) {
-        val (contriesCount, _) = br.readLine()!!.split(" ").map { it.toInt() }
-        repeat(contriesCount - 1) {
-            br.readLine()!!
-        }
-        bw.write("${contriesCount - 1}\n")
+    val nodeCount = br.readLine()!!.toInt()
+    val nodes = MutableList(nodeCount + 1) { mutableListOf<Int>() }
+    var nodesDepthSum = 0
+
+    repeat(nodeCount - 1) {
+        val (node, node2) = br.readLine()!!.split(" ").map { it.toInt() }
+        nodes[node].add(node2)
+        nodes[node2].add(node)
     }
+
+    // 모든 (리프 노드에서 루트 노드까지의 길이)가 짝수라면 패배하고, 홀수라면 이김
+    fun dfs(node: Int = 1, parent: Int = 0, childrenCount: Int = 0) {
+        for (newNode in nodes[node]) {
+            if (newNode != parent) {
+                dfs(node = newNode, parent = node, childrenCount = childrenCount + 1)
+            }
+        }
+
+        if (nodes[node].size == 1) { // 리프 노드일 때
+            nodesDepthSum += childrenCount
+        }
+    }
+
+    dfs()
+    bw.write(if (nodesDepthSum % 2 == 0) "No" else "Yes")
 
     br.close()
     bw.flush()
