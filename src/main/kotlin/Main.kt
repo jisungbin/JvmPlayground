1815b6be8b035978312e3ea692172be5f88bb7ee
jisungbin <ji@sungb.in>
2021-12-31 16:48:44 +0900

    난 수학문제가 싫다

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,52 +2,41 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.LinkedList
+import java.util.Queue
+
+private const val MAX = 100001
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    var taskCount = 0
-    val (nodeCount, edgeCount) = br.readLine()!!.split(" ").map { it.toInt() }
-    val parents = MutableList(nodeCount + 1) { it }
+    val (me, sister) = br.readLine()!!.split(" ").map { it.toInt() }
+    val bfsQueue: Queue<Int> = LinkedList()
+    val visitTimeAndCount = List(MAX) { mutableListOf(-1, 0) }
 
-    fun findParent(node: Int): Int {
-        return if (parents[node] == node) {
-            node
-        } else {
-            val parent = findParent(parents[node])
-            parents[node] = parent
-            parent
-        }
-    }
-// 다른 맥에서 공부하는 중
-    fun unionParent(node: Int, node2: Int) {
-        val nodeParent = findParent(node)
-        val node2Parent = findParent(node2)
-        if (nodeParent == node2Parent) return
-        if (nodeParent < node2Parent) {
-            parents[node2Parent] = nodeParent
-        } else {
-            parents[nodeParent] = node2Parent
-        }
-    }
+    bfsQueue.offer(me)
+    visitTimeAndCount[me][0] = 0 // 방문 시간
+    visitTimeAndCount[me][1] = 1 // 방문 가능한 방법의 수
 
-    repeat(edgeCount) {
-        val (node, node2) = br.readLine()!!.split(" ").map { it.toInt() }
-        if (findParent(node) == findParent(node2)) {
-            taskCount++
-        }
-        unionParent(node, node2)
-    }
-
-    for (i in 1 until nodeCount) {
-        if (findParent(i) != findParent(i + 1)) {
-            unionParent(i, i + 1)
-            taskCount++
+    // sister 에 방문했을 때 멈추는게 아닌, 모든 곳에 다 방문하고 기록함
+    while (bfsQueue.isNotEmpty()) {
+        val _me = bfsQueue.poll()
+        val meVisitTimeAndCount = visitTimeAndCount[_me]
+        for (next in listOf(_me * 2, _me + 1, _me - 1)) {
+            if (next in 0 until MAX) {
+                if (visitTimeAndCount[next][0] == -1) { // 만약 방문한 적이 없다면
+                    bfsQueue.offer(next)
+                    visitTimeAndCount[next][0] = meVisitTimeAndCount[0] + 1
+                    visitTimeAndCount[next][1] = meVisitTimeAndCount[1]
+                } else if (visitTimeAndCount[next][0] == meVisitTimeAndCount[0] + 1) { // 만약 방문한 적이 있다면
+                    visitTimeAndCount[next][1] += meVisitTimeAndCount[1] // 방문 가능한 방법의 수 업데이트
+                }
+            }
         }
     }
 
-    bw.write("$taskCount")
+    bw.write(visitTimeAndCount[sister][0].toString())
 
     br.close()
     bw.flush()
