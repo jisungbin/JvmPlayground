69c4681cb4152cbfb54b91776e871af1bbd9b5fa
jisungbin <ji@sungb.in>
2022-01-09 01:49:33 +0900

    doit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ import java.util.LinkedList
 import java.util.Queue
 import kotlin.math.max
 
-private data class Node(val node: Int, val weight: Int = 0)
+private data class Node(var node: Int, var weight: Int = 0)
 
 @Suppress("BlockingMethodInNonBlockingContext")
 fun main() {
@@ -25,20 +25,21 @@ fun main() {
     fun bfs(node: Int, ignoreNode: Int = 0): Node {
         val bfsQueue: Queue<Node> = LinkedList()
         val vis = MutableList(nodeCount + 1) { false }
-        var largestWeigthNode = Node(node)
+        val largestWeigthNode = Node(node)
         bfsQueue.offer(largestWeigthNode)
-        vis[ignoreNode] = true
 
         while (bfsQueue.isNotEmpty()) {
-            val (_node, _) = bfsQueue.poll()
+            val (_node, _weight) = bfsQueue.poll()
             vis[_node] = true
+
+            if (_node != ignoreNode && _weight > largestWeigthNode.weight) {
+                largestWeigthNode.node = _node
+                largestWeigthNode.weight = _weight
+            }
 
             for (newNode in nodes[_node]) {
                 if (!vis[newNode.node]) {
-                    if (newNode.weight > largestWeigthNode.weight) {
-                        largestWeigthNode = newNode
-                    }
-                    bfsQueue.offer(newNode)
+                    bfsQueue.offer(newNode.apply { weight += _weight })
                 }
             }
         }
@@ -47,7 +48,7 @@ fun main() {
     }
 
     val node = bfs(1, 0) // 제일 끝에 있는 노드
-    val node2 = bfs(node.node, 0) // 제일 끝에 있는 노드 2
+    val node2 = bfs(node.node) // 제일 끝에 있는 노드 2
 
     val result = bfs(node.node, node2.node)
     val result2 = bfs(node2.node, node.node)
