7a2d2c7e0df009dbece0372ec69de851c55ba3ab
jisungbin <ji@sungb.in>
2022-01-17 14:17:06 +0900

    난 수학 문제가 싫다.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,68 @@
-interface Vehicle {
-    fun drive(): String
-}
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
-class CarImpl(val where: String) : Vehicle {
-    override fun drive() = "is going to $where"
-}
+private const val DefaultMapValue = -1
 
-class AirplaneImpl(val where: String) : Vehicle {
-    override fun drive() = "is flying to $where"
-}
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-class CarOrAirplace(val model: String, impl: Vehicle) : Vehicle by impl {
-    fun tellMeYourTrip() {
-        println("$model ${drive()}")
+    val N = br.readLine()!!.toInt()
+    val findN = br.readLine()!!.toInt()
+    var number = N * N + 1
+    val map = List(N) { MutableList(N) { DefaultMapValue } }
+
+    var step = 1
+    var columnIndex = 0
+    var rowIndex = 0
+
+    repeat(N * N) {
+        // 1. columnIndex를 +1 함 -> 만약 (N-1)번째 index라 범위 초과면 2번
+        // 2. rowIndex를 + 1 함 -> 만약 (N-1)번째 index라 범위 초과면 3번
+        // 3. columnIndex가 (N-1)번째 index 이므로 column - 1 함 -> 만약 0번째 index면 4번
+        // 4. columnIndex가 0, rowIndex가 (N-1)번째 이므로 row - 1 함 -> 만약 이미 값이 들어가 있으면 해당 위치에서 1번 부터 반복
+
+        when (step) {
+            1 -> {
+                map[columnIndex][rowIndex] = --number
+                if (columnIndex != N - 1 && map[columnIndex + 1][rowIndex] == DefaultMapValue) { // 마지막 column이 아니고, 다음 column을 방문하지 않았을 때
+                    columnIndex++
+                } else {
+                    step = 2
+                }
+            }
+            2 -> {
+                map[columnIndex][rowIndex] = --number
+                if (rowIndex != N - 1 && map[columnIndex][rowIndex + 1] == DefaultMapValue) { // 마지막 row가 아니고, 다음 row를 방문하지 않았을 때
+                    rowIndex++
+                } else {
+                    step = 3
+                }
+            }
+            3 -> {
+                map[columnIndex][rowIndex] = --number
+                if (columnIndex != 0 && map[columnIndex - 1][rowIndex] == DefaultMapValue) { // 첫 번째 column이 아니고, 이전 column을 방문하지 않았을 때
+                    columnIndex--
+                } else {
+                    step = 4
+                }
+            }
+            4 -> {
+                map[columnIndex][rowIndex] = --number
+                if (rowIndex != 0 && map[columnIndex][rowIndex - 1] == DefaultMapValue) { // 첫 번째 row가 아니고, 이전 row를 방문하지 않았을 때
+                    rowIndex--
+                } else {
+                    step = 1
+                }
+            }
+        }
     }
-}
 
-fun main(args: Array<String>) {
-    val myAirbus330 = CarOrAirplace("Lamborghini", CarImpl("Seoul"))
-    val myBoeing337 = CarOrAirplace("boeing 33u7", AirplaneImpl("Seoul"))
+    bw.write(map.joinToString("\n") { it.joinToString(" ") })
 
-
-    myAirbus330.tellMeYourTrip()
-    myBoeing337.tellMeYourTrip()
+    br.close()
+    bw.flush()
+    bw.close()
 }
