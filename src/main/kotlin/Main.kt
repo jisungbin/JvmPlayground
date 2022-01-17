47a1cf0f64b7a70d3a8e8134a1603f144f7def1a
jisungbin <ji@sungb.in>
2022-01-17 16:45:01 +0900

    난 수학 문제가 싫다.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,57 +10,23 @@ fun main() {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
     val N = br.readLine()!!.toInt()
-    val findN = br.readLine()!!.toInt()
-    var number = N * N + 1
-    val map = List(N) { MutableList(N) { DefaultMapValue } }
+    val cows = mutableMapOf<Int, Int>()
+    var cowMoveCount = 0
 
-    var step = 1
-    var columnIndex = 0
-    var rowIndex = 0
-
-    repeat(N * N) {
-        // 1. columnIndex를 +1 함 -> 만약 (N-1)번째 index라 범위 초과면 2번
-        // 2. rowIndex를 + 1 함 -> 만약 (N-1)번째 index라 범위 초과면 3번
-        // 3. columnIndex가 (N-1)번째 index 이므로 column - 1 함 -> 만약 0번째 index면 4번
-        // 4. columnIndex가 0, rowIndex가 (N-1)번째 이므로 row - 1 함 -> 만약 이미 값이 들어가 있으면 해당 위치에서 1번 부터 반복
-
-        when (step) {
-            1 -> {
-                map[columnIndex][rowIndex] = --number
-                if (columnIndex != N - 1 && map[columnIndex + 1][rowIndex] == DefaultMapValue) { // 마지막 column이 아니고, 다음 column을 방문하지 않았을 때
-                    columnIndex++
-                } else {
-                    step = 2
-                }
-            }
-            2 -> {
-                map[columnIndex][rowIndex] = --number
-                if (rowIndex != N - 1 && map[columnIndex][rowIndex + 1] == DefaultMapValue) { // 마지막 row가 아니고, 다음 row를 방문하지 않았을 때
-                    rowIndex++
-                } else {
-                    step = 3
-                }
-            }
-            3 -> {
-                map[columnIndex][rowIndex] = --number
-                if (columnIndex != 0 && map[columnIndex - 1][rowIndex] == DefaultMapValue) { // 첫 번째 column이 아니고, 이전 column을 방문하지 않았을 때
-                    columnIndex--
-                } else {
-                    step = 4
-                }
-            }
-            4 -> {
-                map[columnIndex][rowIndex] = --number
-                if (rowIndex != 0 && map[columnIndex][rowIndex - 1] == DefaultMapValue) { // 첫 번째 row가 아니고, 이전 row를 방문하지 않았을 때
-                    rowIndex--
-                } else {
-                    step = 1
-                }
+    repeat(N) {
+        val (cow, where) = br.readLine()!!.split(" ").map(String::toInt)
+        val defaultWhere = cows[cow]
+        if (defaultWhere == null) {
+            cows[cow] = where
+        } else {
+            if (defaultWhere != where) {
+                cowMoveCount++
+                cows[cow] = where
             }
         }
     }
 
-    bw.write(map.joinToString("\n") { it.joinToString(" ") })
+    bw.write("$cowMoveCount")
 
     br.close()
     bw.flush()
