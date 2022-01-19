a628d37c6ded08d4d28fbb60c2f883be8a4adac2
jisungbin <ji@sungb.in>
2022-01-19 16:02:11 +0900

    모르겠다;

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,70 @@
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
+import Fakies.random
+import java.lang.reflect.Constructor
+import kotlin.random.Random
 
-private const val DefaultMapValue = -1
+sealed class StringType(val length: Int) {
+    class Korean(length: Int = 10) : StringType(length)
+    class English(length: Int = 10) : StringType(length)
+    class Lipsum(length: Int = 10) : StringType(length)
+}
 
-fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
+@Suppress("MemberVisibilityCanBePrivate")
+object Fakies {
 
-    val N = br.readLine()!!.toInt()
-    val cows = mutableMapOf<Int, Int>()
-    var cowMoveCount = 0
+    @PublishedApi
+    internal fun <T> createEntity(constructor: Constructor<T>, vararg args: Any): T {
+        return constructor.newInstance(*args)
+    }
 
-    repeat(N) {
-        val (cow, where) = br.readLine()!!.split(" ").map(String::toInt)
-        val defaultWhere = cows[cow]
-        if (defaultWhere == null) {
-            cows[cow] = where
-        } else {
-            if (defaultWhere != where) {
-                cowMoveCount++
-                cows[cow] = where
-            }
+    @PublishedApi
+    internal fun randomString(stringRandomType: StringType): String {
+        return when (stringRandomType) {
+            is StringType.Korean -> korean(stringRandomType.length)
+            is StringType.English -> english(stringRandomType.length)
+            is StringType.Lipsum -> LIPSUM.substring(0..stringRandomType.length)
         }
     }
 
-    bw.write("$cowMoveCount")
+    @Suppress("unused")
+    inline fun <reified T> random(
+        count: Int = 1,
+        stringRandomType: StringType = StringType.Lipsum()
+    ): List<T> {
+        return List(count) {
+            val fields = T::class.java.declaredFields
+            val elements = Array(fields.count()) { i ->
+                when (fields[i].type) {
+                    Int::class.java -> Random.nextInt()
+                    Float::class.java -> Random.nextFloat()
+                    Double::class.java -> Random.nextDouble()
+                    Boolean::class.java -> Random.nextBoolean()
+                    String::class.java -> randomString(stringRandomType)
+                    else -> createEntity(fields[i]::class.java.constructors.first())
+                }
+            }
+            createEntity(T::class.java.constructors.first(), *elements) as T
+        }
+    }
 
-    br.close()
-    bw.flush()
-    bw.close()
+    val LIPSUM = """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in 
+        culpa qui officia deserunt mollit anim id est laborum.
+    """.trimIndent()
+
+    private val koreanCharPool = ('가'..'힣')
+    private val englishCharPool = ('a'..'z') + ('A'..'Z')
+
+    fun korean(length: Int = 10) = List(length) { koreanCharPool.random() }.joinToString("")
+    fun english(length: Int = 0) = List(length) { englishCharPool.random() }.joinToString("")
+}
+
+data class Test(val a: Int = 1)
+
+fun main() {
+    println(random<Test>(1))
 }
