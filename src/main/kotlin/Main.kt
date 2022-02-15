c371cfcf0386971d428f556f8403ac99e6903ac9
jisungbin <ji@sungb.in>
2022-02-15 20:29:17 +0900

    코루틴 개어려움;

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,23 @@
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     val flow = flow {
         repeat(100) {
-            delay(300)
+            delay(500)
             emit(it)
         }
-    }.stateIn(this)
-    // state flow: hot
+    }.shareIn(this, SharingStarted.Eagerly)
+    // state flow, shared flow: hot
     // flow: cold
     delay(1000)
-    flow.collect {
+    flow/*.buffer()*/.collect {
+        delay(1000)
         println("collected: $it")
     }
 }
