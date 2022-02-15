b5b804bd26af81656438009e1407e55e3974d6e5
jisungbin <ji@sungb.in>
2022-02-15 20:25:45 +0900

    코루틴 짱어려움;

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,20 @@
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.runBlocking
 
-fun main() {
-    fun String.checkNiceUrl() = Patterns.WEB_URL.matcher(this).matches()
-    val test = """
-        test.test
-        http://naver.com
-        https://www.wwwmasd/a///asdrg#@R7f8uhisdfnjk`213refpv'da;lvxcsD12refdfafafewf
-    """.trimIndent().split("\n")
-    test.forEach {
-        println("$it: ${it.checkNiceUrl()}")
+fun main() = runBlocking {
+    val flow = flow {
+        repeat(100) {
+            delay(300)
+            emit(it)
+        }
+    }.stateIn(this)
+    // state flow: hot
+    // flow: cold
+    delay(1000)
+    flow.collect {
+        println("collected: $it")
     }
 }
