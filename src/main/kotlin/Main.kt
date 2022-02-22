6b8f7a89602aee993d4659e2b26156d47e6ce782
jisungbin <ji@sungb.in>
2022-02-23 04:48:51 +0900

    data class test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,11 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.shareIn
-import kotlinx.coroutines.runBlocking
+data class People(
+    val firstName: String = "",
+    val lastName: String = ""
+) {
+    val isNameStartWithJ = firstName.startsWith("J")
+}
 
-fun main() = runBlocking {
-    val flow = flow {
-        repeat(100) {
-            delay(1000)
-            emit(it)
-        }
-    }
-    // state flow, shared flow: hot
-    // flow: cold
-    delay(1000)
-    flow.buffer().collect {
-        delay(1000)
-        println("collected: $it")
-    }
+fun main() {
+    val people = People()
+    println(people.copy(firstName = "Ji", lastName = "Sungbin").isNameStartWithJ)
 }
