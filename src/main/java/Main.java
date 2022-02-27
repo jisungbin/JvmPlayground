3b5e73483126682207b28d813113182a1be0b824
jisungbin <ji@sungb.in>
2022-02-28 02:23:12 +0900

    Semaphore test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,11 +25,13 @@ class SomeResource {
         try {
             semaphore.acquire(); // Thread 가 semaphore 에게 시작을 알림
 
-            System.out.println("[" + Thread.currentThread().getName() + "]" + (maxThread - semaphore.availablePermits()) + "쓰레드가 점유중");
+            System.out.println("[" + Thread.currentThread().getName() + "] " + (maxThread - semaphore.availablePermits()) + "쓰레드가 점유중");
 
             // semaphore.availablePermits() 사용가능한 Thread 의 숫자
 
-            Thread.sleep((long) (Math.random() * 10000));
+            Thread.sleep(3000);
+
+            System.out.println("--- released ---");
 
             semaphore.release(); // Thread 가 semaphore 에게 종료를 알림
         } catch (InterruptedException e) {
