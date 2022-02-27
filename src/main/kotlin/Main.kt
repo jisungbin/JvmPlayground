39d4e134eb4dcece923cc76cabd3568ea7cab928
jisungbin <ji@sungb.in>
2022-02-28 01:40:42 +0900

    멀티 쓰레스 테스트

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,42 @@
-data class People(
-    val firstName: String = "",
-    val lastName: String = ""
-) {
-    val isNameStartWithJ = firstName.startsWith("J")
+internal class Test2 {
+
+    /* @Volatile*/    // volatile을 붙임으로써 데이터 불일치를 해결할 수 있다.
+    var value: Value = Value(0, false)
+
+    init {
+        val read = createReadThread()
+        val write = createWriteThread()
+        read.start()
+        Thread.sleep(1000)
+        write.start()
+
+        read.join()
+        write.join()
+
+    }
+
+    private fun createReadThread(): Thread {
+        return Thread {
+            while (value.int < 9 && !value.boolean) {
+            }
+            println("read thread finish")
+        }
+    }
+
+    private fun createWriteThread(): Thread {
+        return Thread {
+            for (i in 0 until 10) {
+                value.int++
+            }
+            value.boolean = true
+            println("write thread finish")
+        }
+    }
+
 }
 
 fun main() {
-    val people = People()
-    println(people.copy(firstName = "Ji", lastName = "Sungbin").isNameStartWithJ)
+    Test2()
 }
+
+data class Value(var int: Int, var boolean: Boolean)
