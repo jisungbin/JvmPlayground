7b35701b74d6ef4f37414d9f6ac0380b39c4a1cb
jisungbin <ji@sungb.in>
2022-03-07 19:49:20 +0900

    kotlin `by` keyword

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,33 @@
-import kotlin.system.measureTimeMillis
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.suspendCancellableCoroutine
-
-fun main() = runBlocking {
-    val runningTime = measureTimeMillis {
-        val jobs = List(5) { index ->
-            suspend {
-                println("$index job start.")
-                delay((index * 1000).toLong())
-                "$index job finished at ${System.currentTimeMillis()}"
-            }
-        }
-        awaitAll(jobs).forEach(::println)
-    }
-    println("measureTime: $runningTime")
+interface Speaker {
+    val subject: String
+    val script: String
+    fun say()
 }
 
-private suspend fun <T> awaitAll(jobs: List<suspend () -> T>) = coroutineScope {
-    jobs.map { job ->
-        suspendCancellableCoroutine<T> {
-            launch {
-                job()
-            }
-        }
+class DroidKnights : Speaker {
+    override val subject = "Jetpack Compose 내부 작동 원리"
+    override val script = """
+        안녕하세요, 저는 $subject 에 대해 발표할 지성빈 입니다.
+        Jetpack Compose 는 작년 7월달에 stable 로 출시되었습니다.
+        ...
+    """.trimIndent()
+
+    override fun say() {
+        println("[$subject] $script")
     }
+}
+
+class Sungbin(private val presentation: Speaker) : Speaker {
+    override val subject = presentation.subject
+    override val script = presentation.script
+    override fun say() {
+        presentation.say()
+    }
+}
+
+class SungbinBy(presentation: Speaker) : Speaker by presentation
+
+fun main() {
+    val sungbin = Sungbin(DroidKnights())
+    sungbin.say()
 }
