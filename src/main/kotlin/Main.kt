122399e70d9424709b28aba2925130f1f18d0a73
jisungbin <ji@sungb.in>
2022-03-09 20:14:35 +0900

    coroutine.................

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,30 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
+fun main() = runBlocking<Unit> {
     val sharedFlow = MutableSharedFlow<Int>()
     val channel = Channel<Int>()
-    emitting(sharedFlow, channel)
-    sharedFlow.collect { println(it) }
-    channel.receiveAsFlow().collect { println(it) }
+    launch {
+        repeat(10) {
+            emitting(it, sharedFlow, channel)
+        }
+    }
+    launch {
+        sharedFlow.collect { println("SharedFlow: $it") }
+    }
+    launch {
+        channel.receiveAsFlow().collect { println("Channel: $it") }
+    }
 }
 
-private suspend fun emitting(flow: MutableSharedFlow<Int>, channel: Channel<Int>) {
-    flow.emit(1)
-    channel.send(1)
+private suspend fun <T> emitting(
+    value: T,
+    flow: MutableSharedFlow<T>,
+    channel: Channel<T>,
+) {
+    flow.emit(value)
+    channel.send(value)
 }
