685e4c1448d851a0888312cea45ffe3045808317
jisungbin <ji@sungb.in>
2022-03-26 22:56:16 +0900

    test equality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+enum class A { B, C }
+sealed class B {
+    data class C(var a: Int = 1) : B()
+    data class D(var a: Int = 1) : B()
+}
+object C
 
-fun main() = runBlocking {
-    launch {
-
-    }
-    Unit
+fun main() {
+    println(B.C().hashCode())
+    println(B.C().hashCode())
+    println(B.C() == B.C())
+    println(B.C() === B.C())
+    println()
+    println(C.hashCode())
+    println(C.hashCode())
+    println(C == C)
+    println(C === C)
 }
