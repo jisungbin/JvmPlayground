4695688e5c29bc612cd0ec0724248e46c7d3fd9b
jisungbin <ji@sungb.in>
2022-03-29 11:40:19 +0900

    test `when`

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,6 @@
-enum class A { B, C }
-sealed class B {
-    data class C(var a: Int = 1) : B()
-    data class D(var a: Int = 1) : B()
-}
-object C
-
 fun main() {
-    println(B.C().hashCode())
-    println(B.C().hashCode())
-    println(B.C() == B.C())
-    println(B.C() === B.C())
-    println()
-    println(C.hashCode())
-    println(C.hashCode())
-    println(C == C)
-    println(C === C)
+    when  {
+        true -> println(1)
+        true -> println(2)
+    }
 }
