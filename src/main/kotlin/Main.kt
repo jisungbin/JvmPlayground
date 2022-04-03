c6f8d4f0ca88dc74d3810471bb72326947ce8863
jisungbin <ji@sungb.in>
2022-04-03 16:09:37 +0900

    test sealed class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
-enum class Main {
-    One, Two
+sealed class SnackbarValue {
+    object Invisible : SnackbarValue()
+    class Visible(
+        val message: String,
+        val autoDismiss: Int? = 3000,
+    ) : SnackbarValue() {
+        override fun hashCode(): Int {
+            return message.hashCode() + autoDismiss.hashCode()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Visible) return false
+            if (hashCode() == other.hashCode()) return true
+            return false
+        }
+    }
+}
+
+fun main() {
+    println(SnackbarValue.Visible("123").hashCode())
+    println(SnackbarValue.Visible("123").hashCode())
 }
