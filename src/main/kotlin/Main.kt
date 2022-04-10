8009ec3a55a5149fdfa8793aebb09b8ead7c9d18
jisungbin <ji@sungb.in>
2022-04-11 02:17:55 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,9 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+fun test(action: () -> String) {
+    print(action())
+}
 
-fun main() = runBlocking {
-    launch {
-        delay(300)
-        println("3")
+fun main() {
+    test {
+        "Bye, world!"
     }
-    launch {
-        delay(100)
-        println("2")
-    }
-    println("1")
 }
