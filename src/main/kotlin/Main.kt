39e4466c7e78dcd307769ab919b1db75afda5dfd
jisungbin <ji@sungb.in>
2022-04-13 03:02:40 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 import kotlin.concurrent.thread
 
+val seq = sequence {
+    println("Generating first")
+    yield(1)
+    println("Generating second")
+    yield(2)
+    println("Generating third")
+    yield(3)
+    println("Done")
+}
+
 fun main() {
     repeat(100) {
         thread {
