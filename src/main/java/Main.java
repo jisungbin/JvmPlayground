32bc6c2abd78b82575c0e7a12fb4059eef8c20c7
jisungbin <ji@sungb.in>
2022-04-13 03:26:02 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,30 @@
+import java.awt.Toolkit;
 import java.util.concurrent.Semaphore;
 
 public class Main {
+
     public static void main(String[] args) {
-        final SomeResource resource = new SomeResource(3);
+        Thread thread = new Thread(() -> {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for (; ; ) {
+                toolkit.beep();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.setDaemon(true); // 데몬 스레드로 설정
+        thread.start();
 
-        for (int i = 1; i <= 10; i++) {
-            Thread t = new Thread(resource::use);
-            t.start();
-        }
-
-    }
-}
-
-class SomeResource {
-    private final Semaphore semaphore;
-    private final int maxThread;
-
-    public SomeResource(int maxThread) {
-        this.maxThread = maxThread;
-        this.semaphore = new Semaphore(maxThread);
-    }
-
-    public void use() {
-        try {
-            semaphore.acquire(); // Thread 가 semaphore 에게 시작을 알림
-
-            System.out.println("[" + Thread.currentThread().getName() + "] " + (maxThread - semaphore.availablePermits()) + "쓰레드가 점유중");
-
-            // semaphore.availablePermits() 사용가능한 Thread 의 숫자
-
-            Thread.sleep(3000);
-
-            System.out.println("--- released ---");
-
-            semaphore.release(); // Thread 가 semaphore 에게 종료를 알림
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("비프음!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
