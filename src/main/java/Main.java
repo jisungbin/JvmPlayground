f36662fb0ed47d12a0c909b8446508d49a2e7aac
jisungbin <ji@sungb.in>
2022-04-13 06:14:05 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,28 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.awt.Toolkit;
 import java.util.concurrent.Semaphore;
 
+import kotlin.Function;
+import kotlin.ResultKt;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+import kotlin.coroutines.CoroutineContext;
+import kotlin.coroutines.intrinsics.IntrinsicsKt;
+import kotlin.jvm.functions.Function2;
+import kotlin.jvm.internal.Intrinsics;
+import kotlinx.coroutines.BuildersKt;
+import kotlinx.coroutines.DelayKt;
+
 public class Main {
     public static void main(String[] args) {
-        test: {
-            System.out.println("Hello!");
-            break test;
-        }
-        System.out.println("World!");
+        Function<Void> function = new Function<Void>() {
+            public void returnTest() {
+                return;
+            }
+        };
     }
 }
+
+
