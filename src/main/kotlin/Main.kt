cbfbd361129e68d2a0c4cce094e531eefda76c70
jisungbin <ji@sungb.in>
2022-04-13 05:47:29 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    val one = getOne()
-    val two = getTwo()
-    println("one + two: ${one + two}")
-}
-
-suspend fun getOne(): Int {
-    delay(1000)
-    return 1
-}
-
-suspend fun getTwo(): Int {
-    delay(1000)
-    return 2
+    delay(100)
+    println("Bye, world!")
 }
