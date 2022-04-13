49d1a589237779c6d6271a157e590d68acf98494
jisungbin <ji@sungb.in>
2022-04-13 14:57:29 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
-import kotlin.coroutines.suspendCoroutine
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    suspendCoroutine {
-
-    }
+    delay(100)
+    println("Bye, world!")
 }
