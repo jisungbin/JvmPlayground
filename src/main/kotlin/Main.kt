4e96e7a415d013d62bb5ccffc26fc3d025a67885
jisungbin <ji@sungb.in>
2022-04-14 17:42:42 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
-    delay(100)
-    println("Bye, world!")
+fun main(): Unit = runBlocking {
+//    val user = readUser()
+    b()
+    b()
+    b()
+    println("user")
+}
+
+suspend fun b() {
+    for (i in 1..10) {
+        c(i)
+    }
+}
+
+suspend fun c(i: Int) {
+    delay(i * 100L)
+    println("Tick")
 }
