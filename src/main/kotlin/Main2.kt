628cc9a37c1a65ff31791ec02b446c6e748e9e0a
jisungbin <ji@sungb.in>
2022-04-14 23:21:49 +0900

    test

--- src/main/kotlin/Main2.kt
+++ src/main/kotlin/Main2.kt
@@ -1,6 +1,9 @@
 fun main() {
-    while (true) {
-        println("W")
-        return
-    }
+    val data = mutableListOf(0).also(::plus)
+    println(data)
+}
+
+fun plus(data: MutableList<Int>) {
+    data.add(1)
+    data + 2
 }
