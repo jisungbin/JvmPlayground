daf9dbcc45eed3a5ca081239e1729f222d23e41b
jisungbin <ji@sungb.in>
2022-04-14 18:33:51 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,26 @@
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 
+@OptIn(DelicateCoroutinesApi::class)
 fun main() {
-    var i = 0
-    while (i < 10) {
-        try {
-            println(i)
-            if (++i > 5) return
-        } catch (ignored: Exception) {
-
-        }
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+        println("0: " + Thread.currentThread().name)
     }
-    println("while finished.")
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+        println("1: " + Thread.currentThread().name)
+    }
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+        println("2: " + Thread.currentThread().name)
+    }
+    println("Hello, ")
+    println(Thread.currentThread().name)
+    Thread.sleep(2000L)
 }
