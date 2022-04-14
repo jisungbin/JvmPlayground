39e0974cfae74fca1763c8680ff273346496d43e
jisungbin <ji@sungb.in>
2022-04-14 23:19:08 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,18 @@
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 
-fun main() {
-    GlobalScope.launch {
-
+suspend fun main() = coroutineScope {
+    launch {
+        delay(1000)
+        println("1000 ms sleep.")
+        println("Inner job: ${coroutineContext.job}")
     }
+    println("start sleeping")
+    println("Outer job: ${coroutineContext.job}")
+    println("Outer job childrens: ${coroutineContext.job.children.joinToString(", ")}")
 }
