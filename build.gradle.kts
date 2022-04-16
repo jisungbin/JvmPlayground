eb2a3e6b089bb98c6851e300cb14c527779d052d
jisungbin <ji@sungb.in>
2022-04-16 16:48:02 +0900

    Chore: bump kotlin and change project version name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.20"
     application
 }
 
-group = "io.github.jisungbin"
-version = "1.0-SNAPSHOT"
+group = "land.sungbin"
+version = "1.0-Playground"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
-    implementation("me.moallemi.tools:kotlin-date-range:1.0.0")
 }
 
 tasks.withType<KotlinCompile> {
