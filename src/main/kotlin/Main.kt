49c6f617688e6ea25c38affee9b0140d5c1c6db7
jisungbin <ji@sungb.in>
2022-04-17 05:38:55 +0900

    test sequence

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
+    getSequence().forEach(::println)
+}
 
+fun getSequence(): Sequence<String> = sequence {
+    repeat(3) {
+//        delay(1000) // Compilation error
+        yield("User$it")
+    }
 }
