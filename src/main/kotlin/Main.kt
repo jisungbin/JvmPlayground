0e3616ff32c3031e1359845dec057b5f5624acbc
jisungbin <ji@sungb.in>
2022-04-17 04:16:13 +0900

    test coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,35 @@
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 
-@OptIn(ExperimentalCoroutinesApi::class)
-suspend fun main(): Unit = coroutineScope {
-    produce {
-        repeat(100) { value ->
-            delay(500)
-            send(value)
-        }
-    }.also { channel ->
-        repeat(3) { index ->
-            launch {
-                for (value in channel) {
-                    println("Receive value in $index coroutine: $value")
-                }
-            }
+private fun CoroutineScope.makeChannel() = produce { println("Channel started")
+    for (i in 1..3) {
+        delay(1000)
+        send(i) }
+}
+
+suspend fun main() = runBlocking {
+    val l = product {
+        repeat(3) {
+            add("User$it")
+            println("L: Added User")
         }
     }
+    val l2 = l.map {
+        println("L: Processing")
+        "Processed $it"
+    }
+    val s = sequence {
+        repeat(3) {
+            yield("User$it")
+            println("S: Added User")
+        }
+    }
+    val s2 = s.map {
+        println("S: Processing")
+        "Processed $it"
+    }
+    println("s1: $l2")
+    println("s2: $s2")
 }
