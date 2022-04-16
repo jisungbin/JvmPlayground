a40bd3e72bf4dd89db6da31b979a704a48640bce
jisungbin <ji@sungb.in>
2022-04-17 08:05:53 +0900

    Test val override with var

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,7 @@
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.shareIn
-import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.launch
+interface Test {
+    val value: Int
+}
 
-suspend fun main(): Unit = coroutineScope {
-    val flow = flowOf("A", "B", "C", "D")
-        .onStart { println("Started") }
-        .onCompletion { println("Finished") }
-        .onEach { delay(1000) }
-    val sharedFlow = flow.shareIn(
-        scope = this,
-        started = SharingStarted.WhileSubscribed(),
-    )
-    delay(3000)
-    launch {
-        println("#1 ${sharedFlow.first()}")
-    }
-    launch {
-        println("#2 ${sharedFlow.take(2).toList()}")
-    }
-    delay(3000)
-    launch {
-        println("#3 ${sharedFlow.first()}")
-    }
+class TestImpl : Test {
+    override var value: Int = 1
 }
