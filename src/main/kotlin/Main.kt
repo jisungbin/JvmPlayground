1fc81d602a43644af4aec1f76c200730580860ff
jisungbin <ji@sungb.in>
2022-04-16 17:56:14 +0900

    Test exception

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,17 @@
-import kotlin.system.measureTimeMillis
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @OptIn(ExperimentalCoroutinesApi::class)
-suspend fun main(): Unit = coroutineScope {
-    launch {
-        printCoroutinesTime(Dispatchers.IO) // 2,000 ms
-    }
-    launch {
-        val dispatcher = Dispatchers.IO.limitedParallelism(1000) // or 10
-        printCoroutinesTime(dispatcher) // 1,000 ms or 10,000 ms
-    }
-}
-
-@Suppress("BlockingMethodInNonBlockingContext")
-suspend fun printCoroutinesTime(
-    dispatcher: CoroutineDispatcher
-) {
-    val test = measureTimeMillis {
+fun main(): Unit = runBlocking {
+    try {
         coroutineScope {
-            repeat(100) {
-                launch(dispatcher) {
-                    Thread.sleep(1000)
-                    println("Current worker: ${Thread.currentThread().name} for $dispatcher")
-                }
-            }
+            throw Exception("A!")
         }
+    } catch (ignored: Exception) {
     }
-    println("$dispatcher took: $test")
+    launch {
+        println("B!")
+    }
 }
