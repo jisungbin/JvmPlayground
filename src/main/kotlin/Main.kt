b1f061f9280814cdfb03aa10b740c8088b2ce12d
jisungbin <ji@sungb.in>
2022-04-16 16:46:36 +0900

    code cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,17 @@
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.supervisorScope
+import kotlin.random.Random
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-suspend fun main() = supervisorScope {
-    val str1 = async<String> {
-        delay(1000)
-        throw Exception()
+@OptIn(ExperimentalCoroutinesApi::class)
+suspend fun main() = withContext(Dispatchers.Default.limitedParallelism(3)) {
+    repeat(1000) {
+        launch { // or launch(Dispatchers.Default) {
+            // To make it busy
+            List(1000) { Random.nextLong() }.maxOrNull()
+            val threadName = Thread.currentThread().name
+            println("Running on thread: $threadName")
+        }
     }
-    val str2 = async {
-        delay(2000)
-        "Text2"
-    }
-    try {
-        println(str1.await())
-    } catch (e: Exception) {
-        println(e)
-    }
-    println(str2.await())
 }
