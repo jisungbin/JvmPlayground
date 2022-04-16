7a29332de5a1b8383218a8b93977a19cb3f31a3a
jisungbin <ji@sungb.in>
2022-04-17 08:00:05 +0900

    Test flow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,34 @@
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 
 suspend fun main(): Unit = coroutineScope {
-    getFlow().collect {
+    val flow = flowOf("A", "B", "C", "D")
+        .onStart { println("Started") }
+        .onCompletion { println("Finished") }
+        .onEach { delay(1000) }
+    val sharedFlow = flow.shareIn(
+        scope = this,
+        started = SharingStarted.WhileSubscribed(),
+    )
+    delay(3000)
+    launch {
+        println("#1 ${sharedFlow.first()}")
     }
-}
-
-fun getFlow() = flow {
-    emit(1)
+    launch {
+        println("#2 ${sharedFlow.take(2).toList()}")
+    }
+    delay(3000)
+    launch {
+        println("#3 ${sharedFlow.first()}")
+    }
 }
