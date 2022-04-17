ce1476a9ec7fbca076564ccbadc5c2c41eca54f4
jisungbin <ji@sungb.in>
2022-04-17 17:44:46 +0900

    test coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,18 @@
 @file:Suppress("RedundantSuspendModifier")
 
-suspend fun main() {
-    val name = getName()
-    val age = getAge()
-    println("$name, $age")
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val scope = CoroutineScope(Dispatchers.Default)
+    val scope2 = CoroutineScope(Dispatchers.Default)
+    val scope3 = CoroutineScope(Dispatchers.Default)
+    Thread.currentThread().name.also(::println)
+    listOf(scope, scope2, scope3).forEach { eachScope ->
+        eachScope.launch {
+            println(Thread.currentThread().name)
+        }
+    }
 }
-
-suspend fun getName() = "Ji Sungbin"
-
-suspend fun getAge() = 21
