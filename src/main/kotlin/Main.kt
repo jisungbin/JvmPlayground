ba21a67f463bdc446d5da63e5166ab301643ce07
jisungbin <ji@sungb.in>
2022-04-17 22:42:35 +0900

    test coroutine job

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,17 @@
 @file:Suppress("RedundantSuspendModifier")
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
-    val scope = CoroutineScope(Dispatchers.Default)
-    val scope2 = CoroutineScope(Dispatchers.Default)
-    val scope3 = CoroutineScope(Dispatchers.Default)
-    Thread.currentThread().name.also(::println)
-    listOf(scope, scope2, scope3).forEach { eachScope ->
-        eachScope.launch {
-            println(Thread.currentThread().name)
-        }
+suspend fun main(): Unit = coroutineScope {
+    launch {
+        println("Inner job: " + coroutineContext.job)
+        delay(1000)
+        println("1000 ms sleep.")
     }
+    println("Outer job: " + coroutineContext.job)
+    println("Outer job children: " + coroutineContext.job.children.toList())
+    println("Hey, ")
 }
