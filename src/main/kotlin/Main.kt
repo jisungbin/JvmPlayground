20bad5fc4564c624597bc7afa660e6d5de47a154
jisungbin <ji@sungb.in>
2022-04-19 01:59:41 +0900

    test coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 suspend fun main(): Unit = coroutineScope {
     launch {
         println("Slept 0 ms.")
     }
+    launch {
+        fakeCall()
+    }
     println("Sleeping...")
+}
+
+suspend fun fakeCall(): Int {
+    delay(100) // fake delay
+    return 100
 }
