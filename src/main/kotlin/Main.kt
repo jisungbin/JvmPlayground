5fbb70dd25cf74013240645fc4eab2ffbd7b780c
jisungbin <ji@sungb.in>
2022-04-18 20:26:29 +0900

    test BufferedReader

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
+import java.io.BufferedReader
+import java.io.FileReader
+
+fun sequenceOfLines(fileName: String) = sequence {
+    BufferedReader(FileReader(fileName)).use {
+        while (true) {
+            yield(it.readLine() ?: break)
+        }
+    }
+}
+
 fun main() {
-   val list = intArrayOf(1)
-   list.reverse()
+    sequenceOfLines("https://github.com/kotlin/kotlin-coroutines-examples/tree/master/examples/sequence/sequenceOfLines.kt")
+        .forEach(::println)
 }
