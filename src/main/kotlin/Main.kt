65c309c3de3a7559bc0493fdcd4decccb4af23b3
jisungbin <ji@sungb.in>
2022-04-18 21:40:10 +0900

    test coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,9 @@
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 suspend fun main(): Unit = coroutineScope {
     launch {
-        delay(1000)
-        println("Slept 1000 ms.")
+        println("Slept 0 ms.")
     }
-    launch {
-        delay(300)
-        println("Slept 300 ms.")
-    }
-    println("sleeping...")
+    println("Sleeping...")
 }
