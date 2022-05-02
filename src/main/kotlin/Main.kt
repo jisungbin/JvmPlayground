951ca2ad2debd50d095277cda8cd1e7ab7544231
jisungbin <ji@sungb.in>
2022-05-02 20:10:10 +0900

    Test Nothing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,9 @@
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.supervisorScope
-
-/*
-const val SUSPENDED = 1000
-
-interface Continuation {
-    var label: Int
-    fun resume()
+fun returnNothing(): Nothing {
+    println(1)
+    throw Exception()
 }
 
 fun main() {
-    val continuationImpl = object : Continuation {
-        override var label = 0
-        override fun resume() {
-            label++
-        }
-    }
-}
-
-fun continueFunction(continuation: Continuation): Int {
-    continuation.resume()
-}*/
-
-suspend fun main() {
-    println("Value: ${getIntAsync().await()}")
-}
-
-suspend fun getIntAsync() = supervisorScope {
-    delay(100)
-    async {
-        delay(100)
-        1
-    }
+    returnNothing()
+    println(1)
 }
