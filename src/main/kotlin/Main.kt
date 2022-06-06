40c6c77998ab5bb015030549b17763422c56fb28
jisungbin <ji@sungb.in>
2022-06-06 17:25:48 +0900

    Test flow shareIn

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
+
 fun main() {
-    val string = "AA"
-    val string2 = "AA"
-    println(string === string2)
-    println(string.intern())
-    println(string2.intern())
+    val flow = MutableSharedFlow<Boolean>().shareIn(
+        started = SharingStarted.WhileSubscribed(3000),
+        scope = CoroutineScope(Dispatchers.Default)
+    )
 }
