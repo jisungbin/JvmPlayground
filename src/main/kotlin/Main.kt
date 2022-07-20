64f643fde9f3ebc5f6472ba2dc5d884b313ea85c
jisungbin <ji@sungb.in>
2022-07-20 16:49:55 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,3 @@
-
-
 fun main() {
-    println("1".hashCode())
-    println("2".hashCode())
-    println("3".hashCode())
-
-    var value = 1
-    val valueLambda = { println(value) }
-
-    repeat(3) {
-        println(valueLambda.hashCode())
-        value++
-    }
+    println("1" === String(CharArray(1)))
 }
