c2f3ed8d4bf9722859ad6b385a13117bc423457f
jisungbin <ji@sungb.in>
2022-08-05 23:20:33 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
+data class Wrapper(val a: Int, val b: String)
+
 fun main() {
-    val old = mutableListOf(1)
-    val new = old.apply { add(2) }
-    println(old == new)
+    println(Wrapper(1, "1") == Wrapper(2, "1"))
 }
