a03235d22ce3034ffce13c86be544eacf81ab1fe
jisungbin <ji@sungb.in>
2022-08-07 20:01:16 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,28 @@
-class World {
-    val hello = "Hello, world!"
-    fun bye() = "Bye, world!"
+@file:Suppress("MayBeConstant")
+
+// 컴파일 전
+val NonCaptureLambda = { println("bye, world!") }
+
+// 컴파일 후
+class NonCaptureLambda : Function0<Unit> {
+    override fun invoke() {
+        println("bye, world!")
+    }
 }
 
-fun main() {
-    val lambda = { println("Bye, world!") }
-    val hello = World::hello
-    val bye = World::bye
-    println(hello)
-    println(bye)
-    lambda()
+// file scope -> singleton
+val nonCaptureLambdaInstance = NonCaptureLambda()
+
+val name = "Sungbin"
+
+// 컴파일 전
+val CaptureLambda = { println("[$name] bye, world!") }
+
+// 컴파일 후
+class CaptureLambda(private val name: String) : Function0<Unit> {
+    override fun invoke() {
+        println("[$name] bye, world!")
+    }
 }
+
+// 매번 사용마다 CaptureLambda(name) 을 통해 새로운 인스턴스를 생성 후 invoke 함
