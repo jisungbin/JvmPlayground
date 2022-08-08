4e24a47c9530ac1508c9de95d0b40c602a9edab1
jisungbin <ji@sungb.in>
2022-08-09 07:27:11 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,1 +1,14 @@
-// Empty
+class EmptyClass {
+    override fun equals(other: Any?) = other is EmptyClass
+    override fun hashCode() = javaClass.hashCode()
+}
+
+data class Int(val int: kotlin.Int)
+
+class IntClass(val int: kotlin.Int)
+
+fun main() {
+    println(EmptyClass() == EmptyClass())
+    println(Int(1) == Int(1))
+    println(IntClass(1) == IntClass(1))
+}
