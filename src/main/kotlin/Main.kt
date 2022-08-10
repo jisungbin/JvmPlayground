e785cba42c20ad4ec002615746412447893c4722
jisungbin <ji@sungb.in>
2022-08-10 18:27:06 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,4 @@
-class EmptyClass {
-    override fun equals(other: Any?) = other is EmptyClass
-    override fun hashCode() = javaClass.hashCode()
-}
-
-data class Int(val int: kotlin.Int)
-
-class IntClass(val int: kotlin.Int)
-
-fun main() {
-    println(EmptyClass() == EmptyClass())
-    println(Int(1) == Int(1))
-    println(IntClass(1) == IntClass(1))
+class C {
+    val elementList: List<Int>
+        field = mutableListOf()
 }
