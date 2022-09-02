7196b72b66ee190b1ec578037b33c17bedd0faa8
jisungbin <ji@sungb.in>
2022-09-03 06:27:19 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,40 @@
-import java.util.UUID
+import java.io.File
+import java.util.Locale
 
-val String.Companion.One get() = "1"
+private const val QuackIconsPath =
+    "/Users/jisungbin/AndroidStudioProjects/duckie-quack-quack/ui-components/src/main/res/drawable"
+private val quackIcons = File(QuackIconsPath).list()!!.mapNotNull { filename ->
+    filename.removeSuffix(".xml").takeIf { filename.endsWith(".xml") }
+}
 
-fun String.Companion.hi() {
-    println("Hi")
+/*@Stable
+val Search = QuackIcon(
+    drawableId = R.drawable.ic_search_24,
+)*/
+
+private val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
+private val snakeRegex = "_[a-zA-Z]".toRegex()
+
+fun String.snakeToLowerCamelCase() = snakeRegex.replace(this) {
+    it.value.replace("_", "").uppercase()
+}
+
+fun String.snakeToUpperCamelCase() = snakeToLowerCamelCase().replaceFirstChar {
+    if (it.isLowerCase()) {
+        it.titlecase(Locale.getDefault())
+    } else {
+        it.toString()
+    }
 }
 
 fun main() {
-    println(UUID.randomUUID().toString())
-    // 4b4ac8fe-9c2b-4675-bef1-3143d3b3f3d5
+    println(quackIcons.joinToString("\n\n") { iconFileName ->
+        val iconName = iconFileName.substringAfter("_").substringBeforeLast("_").snakeToUpperCamelCase()
+        """
+        |@Stable
+        |val $iconName = QuackIcon(
+        |    drawableId = R.drawable.$iconFileName,
+        |)
+        """.trimMargin()
+    })
 }
