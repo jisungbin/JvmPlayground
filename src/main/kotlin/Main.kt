104714219f29beae6d06ff9f7627f984a3f32da3
jisungbin <ji@sungb.in>
2022-09-03 04:46:59 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,12 @@
-open class Parent(open val name: String = "1")
-interface IParent {
-    val name: String
-}
 
-class Child : IParent, Parent("1") {
-    // 'name' in 'Parent' is final and cannot be overridden
-    override val name = "2"
+
+val String.Companion.One get() = "1"
+
+fun String.Companion.hi() {
+    println("Hi")
 }
 
 fun main() {
-    val child = Child()
-    println(child.name)
+    String.hi()
+    println(String.One)
 }
