066945de8d10d6934e814fe24486c5803545554f
jisungbin <ji@sungb.in>
2022-09-03 04:54:11 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-
+import java.util.UUID
 
 val String.Companion.One get() = "1"
 
@@ -7,6 +7,6 @@ fun String.Companion.hi() {
 }
 
 fun main() {
-    String.hi()
-    println(String.One)
+    println(UUID.randomUUID().toString())
+    // 4b4ac8fe-9c2b-4675-bef1-3143d3b3f3d5
 }
