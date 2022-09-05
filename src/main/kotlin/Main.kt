7d50070ecb917e083f8d21402f364890fb437766
jisungbin <ji@sungb.in>
2022-09-05 15:01:13 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,24 @@
-@file:Suppress("UNCHECKED_CAST")
 
-class Test {
-    val list = listOf("1")
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import kotlin.time.Duration
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(): Unit = runBlocking {
+    launch { suspendSort() }
+    launch { suspendPrint(123) }
 }
 
-fun main() {
-    val testClass = Class.forName("Test")
-    val test = testClass.getDeclaredConstructor().newInstance()
-    val getList = testClass.getDeclaredMethod("getList")
-    val list = getList(test) as List<String>
-    println(list)
+suspend fun suspendPrint(message: Any) {
+    delay(Duration.ZERO)
+    println(message)
+}
+
+suspend fun suspendSort(end: Int = 5_000_000_0): Unit = suspendCoroutine { continuation ->
+    println("sort start")
+    (1..end).sortedDescending()
+    println("sort end")
+    continuation.resume(Unit)
 }
