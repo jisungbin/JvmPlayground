de8c38125668414a15ad7b2c76f7cb70f85e8d0c
jisungbin <ji@sungb.in>
2022-09-12 22:50:24 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,17 @@
-import okhttp3.OkHttpClient
-import okhttp3.Request
-
 fun main() {
-    continuationNetworkCall {
-        println("Network Call Finish")
+    gugudan(
+        print = ::println,
+        finish = { println("Finish GuGuDan!") }
+    )
+}
+
+private fun gugudan(
+    print: (guguResult: String) -> Unit,
+    finish: () -> Unit,
+) {
+    repeat(9) {
+        val guguResult = "${it + 1} * 2 = ${2 * (it + 1)}"
+        print(guguResult) // callback
     }
-}
-
-fun continuationNetworkCall(callcc: () -> Unit) {
-    val client = OkHttpClient()
-    val request = Request.Builder()
-        .url("https://sungb.in")
-        .build()
-    val response = client.newCall(request).execute()
-    println(response.body?.string())
-    callcc()
-}
-
-fun directNetworkCall() {
-    val client = OkHttpClient()
-    val request = Request.Builder()
-        .url("https://sungb.in")
-        .build()
-    val response = client.newCall(request).execute()
-    println(response.body?.string())
+    finish() // cps
 }
