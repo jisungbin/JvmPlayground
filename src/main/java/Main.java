0a29b91798f76bfa13b001d58698456a669afa4a
jisungbin <ji@sungb.in>
2022-09-17 07:17:31 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
+    static int label = (int) ((Object) true);
+
     public static void main(String[] args) {
-        OverrideClass test = new OverrideClass();
-        test.printIsOveride();
+        // label |= Integer.MIN_VALUE;
+        System.out.println(label);
     }
 }
