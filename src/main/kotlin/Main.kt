444bb428f394d909ec352eed3098a116c023b8bd
jisungbin <ji@sungb.in>
2022-09-16 20:07:24 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,4 @@
 fun main() {
-    gugudan(
-        print = ::println,
-        finish = { println("Finish GuGuDan!") }
-    )
-}
-
-private fun gugudan(
-    print: (guguResult: String) -> Unit,
-    finish: () -> Unit,
-) {
-    repeat(9) {
-        val guguResult = "${it + 1} * 2 = ${2 * (it + 1)}"
-        print(guguResult) // callback
-    }
-    finish() // cps
+    println('A'.isUpperCase())
+    println('a'.isUpperCase())
 }
