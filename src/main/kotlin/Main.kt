11e486217383ce951d51aa4af21f592eb752ede0
jisungbin <ji@sungb.in>
2022-09-18 19:08:11 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,20 @@
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    CoroutineScope(Dispatchers.Default).launch {
-        println(getDelayedValue(100, 1))
+    runBlocking {
+        val job = launch {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
+        }
+        delay(1300L) // delay a bit
+        println("main: I'm tired of waiting!")
+        job.cancel() // cancels the job
+        job.join() // waits for job's completion
+        println("main: Now I can quit.")
     }
 }
 
