63d0c1bc1a222bd945d9503e95f2ebdb7cb0063b
jisungbin <ji@sungb.in>
2022-09-18 15:23:51 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
-fun main() {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+fun main() {
+    CoroutineScope(Dispatchers.Default).launch {
+        println(getDelayedValue(100, 1))
+    }
+}
+
+private suspend fun <T> getDelayedValue(
+    delay: Long,
+    value: T,
+): T {
+    delay(delay)
+    return value
 }
