d60f8f6089bf7bacd0c0cf01a21a670c0b8d2f9f
jisungbin <ji@sungb.in>
2022-10-03 03:52:50 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,19 +4,18 @@ private val types = listOf(
     "bump_patch" to "Patch",
 )
 
-private val targets = listOf(
-    "target_lint-core" to "LintCore",
-    "target_lint-quack" to "LintQuack",
-    "target_lint-compose" to "LintCompose",
-    "target_ui-components" to "UiComponents",
-)
-
-fun main() {
+fun artifectVersionBump() {
+    val targets = listOf(
+        "target_lint-core" to "LintCore",
+        "target_lint-quack" to "LintQuack",
+        "target_lint-compose" to "LintCompose",
+        "target_ui-components" to "UiComponents",
+    )
     val steps = mutableListOf<String>()
     types.forEach { type ->
         targets.forEach { target ->
             val step = """
-                |- name: Bump artifect version (type_${type.first}, target_${target.first})
+                |- name: Bump artifect version (type_${type.second}, target_${target.second})
                 |  if: ${"$"}{{
                 |      contains(github.event.pull_request.labels.*.name, '${type.first}') &&
                 |      contains(github.event.pull_request.labels.*.name, '${target.first}')
@@ -27,4 +26,21 @@ fun main() {
         }
     }
     println(steps.joinToString("\n\n"))
+}
+
+fun playgroundVersionBump() {
+    val steps = mutableListOf<String>()
+    types.forEach { type ->
+        val step = """
+            |- name: Bump playground ${type.second} version
+            |  if: contains(github.event.pull_request.labels.*.name, '${type.first}')
+            |  run: ./gradlew bumpVersion -Ptype="${type.second}" -Ptarget="Playground"
+            """.trimMargin()
+        steps.add(step)
+    }
+    println(steps.joinToString("\n\n"))
+}
+
+fun main() {
+    artifectVersionBump()
 }
