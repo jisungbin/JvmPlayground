5baf59eee383e92dc72ae1d617c53ef2fa9048bd
jisungbin <ji@sungb.in>
2022-10-03 01:12:47 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,42 @@
-fun main() {
-    val list = listOf("안녕", "안녕녕", "안녕녕녕", "안녕녕녕녕녕녕녕녕", "안")
-    val index = list.maxBy { item ->
-        item.length
-    }.let { item ->
-        list.indexOf(item)
+import java.io.File
+
+val file = File("/Users/jisungbin/AndroidStudioProjects/duckie-quack-quack/playground-version.txt")
+// major=0
+// minor=1
+// patch=0
+
+enum class VersionType {
+    Majer,
+    Minor,
+    Patch;
+}
+
+fun bumpVersion(
+    type: VersionType,
+): String {
+    val lines = file.readLines().toMutableList()
+    when (type) {
+        VersionType.Majer -> {
+            val major = lines[0].split("=")[1].toInt()
+            lines[0] = "major=${major + 1}"
+        }
+        VersionType.Minor -> {
+            val minor = lines[1].split("=")[1].toInt()
+            lines[1] = "minor=${minor + 1}"
+        }
+        VersionType.Patch -> {
+            val patch = lines[2].split("=")[1].toInt()
+            lines[2] = "patch=${patch + 1}"
+        }
     }
-    println(index)
-    println(list[index])
+    return lines.joinToString(
+        separator = "\n",
+    )
+}
+
+fun main() {
+    VersionType.values().forEach {
+        println(bumpVersion(it))
+        println("-----------")
+    }
 }
