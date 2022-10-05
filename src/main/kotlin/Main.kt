853dce086080f429059ed99c7be2a7b59d4cb56b
jisungbin <ji@sungb.in>
2022-10-05 18:46:55 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    Job()
+    runBlocking(
+        context = CoroutineExceptionHandler { coroutineContext, throwable ->
+            println("Caught $throwable in $coroutineContext")
+        },
+    ) {
+        val job = launch {
+            delay(1000)
+        }
+        launch {
+            try {
+                job.cancel()
+                job.join()
+            } catch (exception: Exception) {
+                println("Caught $exception")
+            }
+        }
+    }
 }
