e7f839b6b647d23024ac7f9c96f9e80bf7cc9760
jisungbin <ji@sungb.in>
2022-10-07 04:17:51 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
 @SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        System.out.println("20221003 지성빈 반복");
-        int i = 1, sum = 0;
-        do {
-            sum += i++;
-        } while (i <= 100);
-        System.out.println("1 부터 100 까지 합 = " + sum);
+
     }
 }
