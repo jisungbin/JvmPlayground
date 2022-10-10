d32e0773ad0bc55469cf958e0e56dbcc756d0f5f
jisungbin <ji@sungb.in>
2022-10-11 05:16:02 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,76 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import kotlin.ranges.IntProgression;
+import kotlin.ranges.IntRange;
+import kotlin.ranges.RangesKt;
+
 @SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        System.out.println("20221003 지성빈 반복");
-        int i = 1, sum = 0;
-        do {
-            sum += i++;
-        } while (i <= 100);
-        System.out.println("1 부터 100 까지 합 = " + sum);
+        System.out.println("20221011 지성빈 배열");
+        byte var0 = 1;
+        Iterable range = (Iterable)(new IntRange(var0, 30));
+        Collection destination = (Collection)(new ArrayList());
+        Iterator iterator = range.iterator();
+
+        while(iterator.hasNext()) {
+            Object next = iterator.next();
+            int p1 = ((Number)next).intValue();
+            if (checkPrime(p1)) {
+                destination.add(next);
+            }
+        }
+
+        range = (Iterable)((List)destination);
+        Iterator var2 = range.iterator();
+
+        while(var2.hasNext()) {
+            Object next = var2.next();
+            int p1 = ((Number)next).intValue();
+            System.out.println(p1);
+        }
+    }
+
+    private static final boolean checkPrime(int n) {
+        if (n <= 1) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else if (n % 2 == 0) {
+            return false;
+        } else {
+            boolean flag = true;
+            double var3 = (double) n;
+            int root = (int) Math.sqrt(var3);
+            byte var6 = 3;
+            IntProgression step = RangesKt.step((IntProgression) (new IntRange(var6, root)), 2);
+            int i = step.getFirst();
+            int var4 = step.getLast();
+            int var5 = step.getStep();
+            if (var5 >= 0) {
+                if (i > var4) {
+                    return flag;
+                }
+            } else if (i < var4) {
+                return flag;
+            }
+
+            while (true) {
+                if (n % i == 0) {
+                    flag = false;
+                    break;
+                }
+
+                if (i == var4) {
+                    break;
+                }
+
+                i += var5;
+            }
+
+            return flag;
+        }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,36 @@
-import java.util.*
+import kotlin.math.sqrt
 
 fun main() {
-    UUID.randomUUID()
+    (1..30).filter(::checkPrime).forEach(::println)
+}
+
+private fun checkPrime(n: Int): Boolean {
+    // Number less than or equal 1 is not prime
+    if (n <= 1) {
+        return false
+    }
+
+    // 2 is only even prime number
+    if (n == 2) {
+        return true
+    }
+
+    // Check if number (> 2) is even. If yes, it is not prime.
+    if (n % 2 == 0) {
+        return false
+    }
+
+    var flag = true
+
+    val root = sqrt(n.toDouble()).toInt()
+
+    // We need to check till square root of n only to find whether n is prime.
+    for (i in 3..root step 2) {
+        if ((n % i) == 0) {
+            flag = false
+            break
+        }
+    }
+
+    return flag
 }
