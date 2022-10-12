441cf5a2a5fc5fd9cff25b3a3d687a395a7d5e4e
jisungbin <ji@sungb.in>
2022-10-13 07:13:13 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,26 @@
 @SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        System.out.println("20221011 지성빈 함수");
-        System.out.println(genderToKor("female"));
-        System.out.println(genderToKor("male"));
+        System.out.println("20221011 지성빈 배열");
+
+        for (int i = 1; i <= 30; i++) {
+            printIfPrime(i);
+        }
     }
 
-    static String genderToKor(String gender) {
-        if (gender == "female") return "여자";
-        else return "남자";
+    public static void printIfPrime(int number) {
+        if (number < 2) {
+            return;
+        }
+        if (number == 2) {
+            System.out.print(number + " ");
+            return;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return;
+            }
+        }
+        System.out.print(number + " ");
     }
 }
