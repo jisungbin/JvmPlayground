c15d267994b2191cdc3cc3c4226a1be0800e508a
jisungbin <ji@sungb.in>
2022-10-19 00:25:09 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,13 @@
 @SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        System.out.println("20221011 지성빈 배열");
-
-        for (int i = 1; i <= 30; i++) {
-            printIfPrime(i);
-        }
+        System.out.println("20221019 지성빈 힘수");
+        int x1 = 10, y1 = 30, x2 = 20, y2 = 60;
+        double length = calcLength(x1, y1, x2, y2);
+        System.out.println("두 점 사이의 거리는 " + length + "입니다.");
     }
 
-    public static void printIfPrime(int number) {
-        if (number < 2) {
-            return;
-        }
-        if (number == 2) {
-            System.out.print(number + " ");
-            return;
-        }
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return;
-            }
-        }
-        System.out.print(number + " ");
+    private static double calcLength(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
