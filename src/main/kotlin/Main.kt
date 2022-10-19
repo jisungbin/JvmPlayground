a2d865d05670a56e6684bc502c29ed41aa08c5cd
jisungbin <ji@sungb.in>
2022-10-20 04:33:51 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,21 @@
-sealed interface DuckApiResult<T> {
-    data class Exception<T>(val message: String?) : DuckApiResult<T>
-}
+@RequiresOptIn(
+    message = "This API is experimental and may change in the future.",
+)
+@Target(
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.CONSTRUCTOR,
+)
+annotation class Unsupported
 
-sealed class DuckFetchResult<T> : DuckApiResult<T> {
-    data class Success<T>(val value: T) : DuckFetchResult<T>()
-    class Empty<T> : DuckFetchResult<T>()
-}
-
-sealed class DuckUpsertResult<T> : DuckApiResult<T> {
-    class Done<T> : DuckUpsertResult<T>()
-}
+data class Test(
+    @Unsupported val new: Int,
+    val old: Int,
+)
 
 fun main() {
-    val exception: DuckApiResult<Int> = DuckApiResult.Exception("11")
-    val success: DuckApiResult<Int> = DuckFetchResult.Success(1)
-    println(
-        when (exception) {
-            is DuckApiResult.Exception -> "DuckApiException: ${exception.message}"
-            is DuckFetchResult.Success -> "DuckFetchResult.Success: ${exception.value}"
-            is DuckFetchResult.Empty -> "DuckFetchResult.Empty"
-            is DuckUpsertResult.Done -> "DuckUpsertResult.Done"
-        }
+    val test = Test(
+        new = 1,
+        old = 1
     )
+    test.new
 }
