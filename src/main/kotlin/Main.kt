7beb64f409a12ff83c971cff26ca077da92fcab2
jisungbin <ji@sungb.in>
2022-10-19 23:58:05 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,19 @@
-import kotlin.math.sqrt
+sealed interface DuckApiResult<T>
 
-fun main() {
-    (1..30).filter(::checkPrime).forEach(::println)
+class DuckApiException(override val message: String?) : Exception(), DuckApiResult<Nothing>
+
+sealed class DuckFetchResult : DuckApiResult<Nothing> {
+    data class Success<T>(val value: T) : DuckApiResult<T>
+    object Empty : DuckApiResult<Nothing>
 }
 
-private fun checkPrime(n: Int): Boolean {
-    // Number less than or equal 1 is not prime
-    if (n <= 1) {
-        return false
-    }
-
-    // 2 is only even prime number
-    if (n == 2) {
-        return true
-    }
-
-    // Check if number (> 2) is even. If yes, it is not prime.
-    if (n % 2 == 0) {
-        return false
-    }
-
-    var flag = true
-
-    val root = sqrt(n.toDouble()).toInt()
-
-    // We need to check till square root of n only to find whether n is prime.
-    for (i in 3..root step 2) {
-        if ((n % i) == 0) {
-            flag = false
-            break
+fun main() {
+    val result: DuckApiResult<Nothing> = DuckApiException(null)
+    println(
+        when (result) {
+            is DuckApiException -> "DuckApiException"
+            is DuckFetchResult.Success -> "DuckFetchResult.Success: ${result.value}"
+            is DuckFetchResult.Empty -> "DuckFetchResult.Empty"
         }
-    }
-
-    return flag
+    )
 }
