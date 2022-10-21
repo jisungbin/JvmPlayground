98771cd7741111177ee3527d647dc87c57b983f1
jisungbin <ji@sungb.in>
2022-10-21 15:52:56 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,29 @@
-@RequiresOptIn(
-    message = "This API is experimental and may change in the future.",
-)
-@Target(
-    AnnotationTarget.PROPERTY,
-    AnnotationTarget.CONSTRUCTOR,
-)
-annotation class Unsupported
-
-data class Test(
-    @Unsupported val new: Int,
-    val old: Int,
-)
+fun publishSteps() {
+    val targets = listOf(
+        "ReleaseTarget_LintCore" to "lint-core-publish",
+        "ReleaseTarget_LintQuack" to "lint-quack-publish",
+        "ReleaseTarget_LintCompose" to "lint-compose-publish",
+        "ReleaseTarget_LintWriting" to "lint-writing-publish",
+        "ReleaseTarget_UiComponents" to "ui-components",
+    )
+    val steps = mutableListOf<String>()
+    targets.forEach { target ->
+        val step = """
+            |- name: Publish ${target.second} to MavenCentral
+            |  if: contains(github.event.pull_request.labels.*.name, '${target.first}')
+            |  run: ./gradlew :${target.second}:publish --scan
+        """.trimMargin()
+        steps.add(
+            element = step,
+        )
+    }
+    println(
+        message = steps.joinToString(
+            separator = "\n\n",
+        ),
+    )
+}
 
 fun main() {
-    val test = Test(
-        new = 1,
-        old = 1
-    )
-    test.new
+    publishSteps()
 }
