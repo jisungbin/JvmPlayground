675946b66a68d6f476e0a9901548d51e5048863c
jisungbin <ji@sungb.in>
2022-10-23 12:46:31 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,5 @@
-fun publishSteps() {
-    val targets = listOf(
-        "ReleaseTarget_LintCore" to "lint-core-publish",
-        "ReleaseTarget_LintQuack" to "lint-quack-publish",
-        "ReleaseTarget_LintCompose" to "lint-compose-publish",
-        "ReleaseTarget_LintWriting" to "lint-writing-publish",
-        "ReleaseTarget_UiComponents" to "ui-components",
-    )
-    val steps = mutableListOf<String>()
-    targets.forEach { target ->
-        val step = """
-            |- name: Publish ${target.second} to MavenCentral
-            |  if: contains(github.event.pull_request.labels.*.name, '${target.first}')
-            |  run: ./gradlew :${target.second}:publish --scan
-        """.trimMargin()
-        steps.add(
-            element = step,
-        )
-    }
-    println(
-        message = steps.joinToString(
-            separator = "\n\n",
-        ),
-    )
-}
+data class Test(val asDASADSAASDA__asd: Int)
 
 fun main() {
-    publishSteps()
+    println(Test::asDASADSAASDA__asd.name)
 }
