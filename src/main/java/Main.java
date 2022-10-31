edf40c508474eac88da5b9047d8f3f49cfb431e5
jisungbin <ji@sungb.in>
2022-10-31 20:37:33 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,82 @@
-@SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        System.out.println("20221019 지성빈 힘수");
-        int x1 = 10, y1 = 30, x2 = 20, y2 = 60;
-        double length = calcLength(x1, y1, x2, y2);
-        System.out.println("두 점 사이의 거리는 " + length + "입니다.");
+        System.out.println("20221031 지성빈 클래스");
+
+        Student stud1 = new Student("201901", "홍길동", 3);
+        stud1.exam[0].setExamScore("", 88);
+
+        stud1.exam[0].setSubject("자바");
+        stud1.exam[1].setSubject("C");
+        stud1.exam[2].setSubject("C++");
+        stud1.exam[0].setScore(89);
+        stud1.exam[1].setScore(90);
+        stud1.exam[2].setScore(100);
+        stud1.printGPA();
+    }
+}
+
+class Student {
+    String studNo, name;
+    int totSubject;
+    Exam[] exam;
+
+    public Student(String studNo, String name, int totSubject) {
+        this.studNo = studNo;
+        this.name = name;
+        this.totSubject = totSubject;
+        exam = new Exam[totSubject];
+        for (int i = 0; i < exam.length; i++) exam[i] = new Exam();
     }
 
-    private static double calcLength(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    public void setStudNo(String studNo) {
+        this.studNo = studNo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getGPA() {
+        int sum = 0;
+        for (Exam value : exam) sum = sum + value.getScore();
+        return sum;
+    }
+
+    public void printGPA() {
+        for (Exam value : exam) {
+            System.out.println("과목명:" + value.getSubject() + "점수=" + value.getScore());
+        }
+        System.out.println("과목합계=" + this.getGPA());
+    }
+}
+
+class Exam {
+    String subject;
+    int score;
+
+    public void setExamScore(String subject, int score) {
+        this.subject = subject;
+        this.score = score;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return this.score;
+
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,11 @@
-interface A {
-    val a: Int
-}
+@Target(
+    AnnotationTarget.PROPERTY,
+)
+@RequiresOptIn(
+    message = "이 필드는 MVP 개발 환경에서는 사용되서는 안됩니다.",
+)
+annotation class Unsupported
 
-interface B : A {
-    override val a: Int
-    val b: Int
-}
-
-fun main() {
-    val B = object : B {
-        override val a = 1
-        override val b = 2
-    }
-    val A = B as A
-    println(B.a)
-    println(A.a)
-    println((A as B).b)
-}
+data class Test(
+    @Unsupported val test: Int,
+)
