d8d49bbd2381361eee4d5fe4344aaa74baaab327
jisungbin <ji@sungb.in>
2022-11-01 01:58:30 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 private val _flow = MutableStateFlow(1)
 val flow: StateFlow<Int> get() = _flow
+val flow2 = _flow.asStateFlow()
