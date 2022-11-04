601f180bc355ef972c388837a0337741a0caa7fb
jisungbin <ji@sungb.in>
2022-11-05 03:05:48 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+fun main() {
+    val list = mutableListOf<Int>()
+    test(
+        value = run {
+            println(1)
+            list.add(1)
+            1
+        },
+        value2 = run {
+            list.add(2)
+            println(list)
+            2
+        },
+    )
+}
 
-private val _flow = MutableStateFlow(1)
-val flow: StateFlow<Int> get() = _flow
-val flow2 = _flow.asStateFlow()
+fun test(
+    value: Any,
+    value2: Any,
+) {
+}
