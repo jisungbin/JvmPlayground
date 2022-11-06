136dd37e73be7e77be2623a21c59df733c1ebc74
jisungbin <ji@sungb.in>
2022-11-07 01:45:06 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("20221031 지성빈 클래스");
-        Cat3 aCat = new Cat3();
-        aCat.setName("야옹이");
-        System.out.println(aCat.getName() + "의 장점 = " + aCat.crawling());
-        System.out.println(aCat.getName() + "는 " + aCat.identify());
+        System.out.println("20221107 지성빈 상속");
+        new SmartPhone().display();
     }
 }
 
-class Animal3 {
+abstract class Device {
     String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String identify() {
-        return "동물";
-    }
+    abstract void display();
 }
 
-class Cat3 extends Animal3 {
-    public String crawling() {
-        return "나무오르기";
-    }
-
-    @Override
-    public String identify() {
-        return "고양이";
+class SmartPhone extends Device {
+    void display() {
+        System.out.println("스마트폰 디스플레이");
     }
 }
