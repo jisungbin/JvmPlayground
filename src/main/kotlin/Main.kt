2044c42fe99eadd5b6746de96e1be103d758cc25
jisungbin <ji@sungb.in>
2022-11-10 23:42:17 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,29 @@
-@JvmInline
-value class QuackModifier(internal val data: QuackModifierData = QuackModifierData())
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 
-fun main() {
-    val modifier = QuackModifier()
-        .wrapContent()
-        .fillContent()
-}
+abstract class BaseViewModel<State, SideEffect>(
+    initialState: State,
+) {
+    private val _state = MutableStateFlow(value = initialState)
+    val state = _state.asStateFlow()
 
-fun QuackModifier.wrapContent() = also { data.height = 2 }
-fun QuackModifier.fillContent() = also { data.height = 3 }
+    private val _sideEffect = Channel<SideEffect>()
+    val sideEffect = _sideEffect.receiveAsFlow()
 
-class QuackModifierData {
-    var height = 1
+    protected val currentState get() = _state.value
+
+    protected fun updateState(
+        reducer: State.() -> State,
+    ) {
+        val newState = currentState.reducer()
+        _state.value = newState
+    }
+
+    protected suspend fun postSideEffect(
+        effect: () -> SideEffect,
+    ) {
+        _sideEffect.send(element = effect())
+    }
 }
