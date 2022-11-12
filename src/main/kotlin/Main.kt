5d21492d39a1d6dbfd3c8b1e405fcdf6762dbcc4
jisungbin <ji@sungb.in>
2022-11-12 12:15:47 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,16 @@
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
-
-abstract class BaseViewModel<State, SideEffect>(
-    initialState: State,
-) {
-    private val _state = MutableStateFlow(value = initialState)
-    val state = _state.asStateFlow()
-
-    private val _sideEffect = Channel<SideEffect>()
-    val sideEffect = _sideEffect.receiveAsFlow()
-
-    protected val currentState get() = _state.value
-
-    protected fun updateState(
-        reducer: State.() -> State,
-    ) {
-        val newState = currentState.reducer()
-        _state.value = newState
+fun main() {
+    val rawCode = """
+        |// Kotlin
+        |fun main() {
+        |    println("Hello, Kotlin!")
+        |}
+    """.trimMargin()
+    val codes = rawCode.split("\n").mapIndexed { index, code ->
+        if (index == 0) {
+            code
+        } else {
+            "   $code"
+        }
     }
-
-    protected suspend fun postSideEffect(
-        effect: () -> SideEffect,
-    ) {
-        _sideEffect.send(element = effect())
-    }
+    println(codes.joinToString("\n"))
 }
