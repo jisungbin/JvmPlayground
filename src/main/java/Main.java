426489313455380711fb090c36d8e3fc1dafb572
jisungbin <ji@sungb.in>
2022-11-14 23:16:23 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,41 @@
 @SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println("20221114 지성빈 예외");
-            int throwing =1/0;
-        } catch (Exception e) {
-            System.out.println("예외처리루틴: " + e + "예외 발생");
-        } finally {
-            System.out.println("예외처리 후 finally 블럭 수행함");
-        }
+        System.out.println("20221114 지성빈 인터페이스");
+        KHello4 hello1 = new KHello4();
+        hello1.goodMorning3();
+        hello1.goodNight3();
+        hello1.goodAftermoon3();
+    }
+}
+
+interface GoodMoring3 {
+    void goodMorning3();
+}
+
+interface GoodNight3 {
+    void goodNight3();
+}
+
+class KHello3 implements GoodMoring3, GoodNight3 {
+    @Override
+    public void goodMorning3() {
+        System.out.println("좋은 아침");
+    }
+
+    @Override
+    public void goodNight3() {
+        System.out.println("좋은 저녁");
+    }
+}
+
+interface KSay extends GoodMoring3, GoodNight3 {
+    void goodAftermoon3();
+}
+
+class KHello4 extends KHello3 implements KSay {
+    @Override
+    public void goodAftermoon3() {
+        System.out.println("좋은 점심");
     }
 }
