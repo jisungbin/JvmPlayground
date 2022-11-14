15e2fcf89affab3e13d195a70b2cceb776c90ab9
jisungbin <ji@sungb.in>
2022-11-14 23:11:19 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 @SuppressWarnings("ALL")
-public class  Main {
+public class Main {
     public static void main(String[] args) {
-
+        try {
+            System.out.println("20221114 지성빈 예외");
+            int throwing =1/0;
+        } catch (Exception e) {
+            System.out.println("예외처리루틴: " + e + "예외 발생");
+        } finally {
+            System.out.println("예외처리 후 finally 블럭 수행함");
+        }
     }
 }
