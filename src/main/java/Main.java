0c24cdb574d0d1004d74dd634e2ee2c4e5821237
jisungbin <ji@sungb.in>
2022-11-20 10:54:11 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,40 +2,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("20221114 지성빈 인터페이스");
-        KHello4 hello1 = new KHello4();
-        hello1.goodMorning3();
-        hello1.goodNight3();
-        hello1.goodAftermoon3();
-    }
-}
-
-interface GoodMoring3 {
-    void goodMorning3();
-}
-
-interface GoodNight3 {
-    void goodNight3();
-}
-
-class KHello3 implements GoodMoring3, GoodNight3 {
-    @Override
-    public void goodMorning3() {
-        System.out.println("좋은 아침");
-    }
-
-    @Override
-    public void goodNight3() {
-        System.out.println("좋은 저녁");
-    }
-}
-
-interface KSay extends GoodMoring3, GoodNight3 {
-    void goodAftermoon3();
-}
-
-class KHello4 extends KHello3 implements KSay {
-    @Override
-    public void goodAftermoon3() {
-        System.out.println("좋은 점심");
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
+import java.io.File
+
 fun main() {
-    val rawCode = """
-        |// Kotlin
-        |fun main() {
-        |    println("Hello, Kotlin!")
-        |}
-    """.trimMargin()
-    val codes = rawCode.split("\n").mapIndexed { index, code ->
-        if (index == 0) {
-            code
-        } else {
-            "   $code"
-        }
+    println("20221120 지성빈 스트림")
+    File("/Users/jisungbin/Downloads/i-hate-java.txt").run {
+        createNewFile()
+        writeText("KOREA")
+        println("from text: ${readText()}")
+        println("from byte: ${readBytes().decodeToString()}")
+
+        writeText(
+            """
+                더하기 a+b=15
+                빼기 a-b=5
+            """.trimIndent()
+        )
+        println("from text: ${readText()}")
+        println("from byte: ${readBytes().decodeToString()}")
+
+        delete()
     }
-    println(codes.joinToString("\n"))
 }
