b7d3f84d6356d1a09d8e5c3a5f21a7e9aba2318e
jisungbin <ji@sungb.in>
2022-12-10 01:14:21 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,23 @@
+@file:Suppress("VARIABLE_WITH_REDUNDANT_INITIALIZER")
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-suspend fun main(): Unit = runBlocking {
-    launch {
+suspend fun main() = runBlocking {
+    val deferred = async {
+        println("started - 1")
         delay(1000)
-        println("1000 slept")
-    }.also { job ->
-        job.invokeOnCompletion {
-            println("finished")
-        }
+        println("Done - 1")
+        1000
     }
+    println("job1 status: ${deferred.isActive}")
+    delay(400)
+    deferred.cancelAndJoin()
+    val result = runCatching {
+        deferred.await()
+    }
+    println("job1 status: ${deferred.isActive}")
+    println(result)
 }
