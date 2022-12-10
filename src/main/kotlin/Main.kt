594ce5030b15331a605feb11b74d65c8f3d9a636
jisungbin <ji@sungb.in>
2022-12-11 01:42:08 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 
 import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.supervisorScope
 
-fun main(): Unit = runBlocking {
-    launch {
+suspend fun main() {
+    try {
         supervisorScope {
             val worker = async { error("test") }
             val result = runCatching { worker.await() }
             println(result)
         }
+    } catch (exception: Exception) {
+        println(exception)
     }
 }
