e0917a53e373e38b027e745d4deedbc66fd35d92
jisungbin <ji@sungb.in>
2022-12-25 01:02:23 +0900

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,49 @@
-class Test {
-    private int test;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-    public Test(int test) {
-        this.test = test;
-    }
-}
+/**
+ * Run Configuration used for running Android Apps (and Instant Apps) locally on a device/emulator.
+ */
+public class AndroidRunConfiguration extends AndroidRunConfigurationBase implements RefactoringListenerProvider, RunnerIconProvider, AppRunConfiguration {
+    // ...
 
-class Test2 extends Test {
-    private int test2;
+    public String ACTIVITY_EXTRA_FLAGS = "";
 
-    public Test2(int test, int test2) {
-        super(test);
-        this.test2 = test2;
+    @Nullable
+    @Override
+    protected AppLaunchTask getApplicationLaunchTask(@NotNull ApplicationIdProvider applicationIdProvider,
+                                                     @NotNull AndroidFacet facet,
+                                                     @NotNull String contributorsAmStartOptions,
+                                                     boolean waitForDebugger,
+                                                     @NotNull LaunchStatus launchStatus,
+                                                     @NotNull ApkProvider apkProvider,
+                                                     @NotNull ConsolePrinter consolePrinter,
+                                                     @NotNull IDevice device) {
+        ActivityLaunchOptionState state = getLaunchOptionState(MODE);
+        assert state != null;
+
+        String extraFlags = ACTIVITY_EXTRA_FLAGS;
+        if (!contributorsAmStartOptions.isEmpty()) {
+            extraFlags += (extraFlags.isEmpty() ? "" : " ") + contributorsAmStartOptions;
+        }
+
+        if (device.getVersion().isGreaterOrEqualThan(AndroidVersion.VersionCodes.TIRAMISU)) {
+            extraFlags += (extraFlags.isEmpty() ? "" : " ") + "--splashscreen-show-icon";
+        }
+
+        StartActivityFlagsProvider startActivityFlagsProvider;
+        if (facet.getConfiguration().getProjectType() == PROJECT_TYPE_INSTANTAPP) {
+            startActivityFlagsProvider = new InstantAppStartActivityFlagsProvider();
+        } else {
+            startActivityFlagsProvider = new DefaultStartActivityFlagsProvider(waitForDebugger, extraFlags);
+        }
+
+        try {
+            return state.getLaunchTask(applicationIdProvider.getPackageName(), facet, startActivityFlagsProvider, getProfilerState(), apkProvider);
+        } catch (ApkProvisionException e) {
+            Logger.getInstance(AndroidRunConfiguration.class).error(e);
+            launchStatus.terminateLaunch("Unable to identify application id", true);
+            return null;
+        }
     }
 }
