bc9ef39c9954e08595731be21a86358cd0cede15
jisungbin <ji@sungb.in>
2022-12-28 21:27:27 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,3 @@
-fun main() {
-    val test = CompositionLocalContext.Test
-    test.javaClass.getDeclaredField("test").apply {
-        isAccessible = true
-        println(get(test))
-    }
-}
+const val One = 1
+val getter: Int get() = One
+const val direct = One
