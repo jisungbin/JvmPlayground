f6be30e0a874ff9403e010706775d6420825d911
jisungbin <ji@sungb.in>
2023-01-03 05:41:25 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,21 @@
-import io.ktor.server.application.Application
-import io.ktor.server.application.call
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import io.ktor.server.request.receiveText
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.delay
+@file:Suppress("OPT_IN_USAGE")
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-@OptIn(DelicateCoroutinesApi::class)
-fun main() = runBlocking {
-    val serverJob = launch {
-        embeddedServer(
-            factory = Netty,
-            module = Application::bodyPrintModule,
-        ).start()
-    }
-
-    delay(3000)
-    serverJob.cancelAndJoin()
-}
-
-fun Application.bodyPrintModule() {
-    routing {
-        get("/") {
-            val bodyRawData = call.receiveText()
-            call.respondText(
-                """
-                Hello, world!
-                RawData: $bodyRawData
-                """.trimIndent()
-            )
+fun main() {
+    var number = 0
+    runBlocking {
+        val times = 10_000
+        val worker1 = launch(Dispatchers.IO.limitedParallelism(1)) {
+            repeat(times) { number++ }
         }
+        val worker2 = launch(Dispatchers.IO.limitedParallelism(1)) {
+            repeat(times) { number-- }
+        }
+        joinAll(worker1, worker2)
     }
+    println(number)
 }
