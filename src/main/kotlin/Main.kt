31cd87b11f314cd65b29cc3d64bd9b168a349993
jisungbin <ji@sungb.in>
2023-01-03 06:00:29 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,20 @@
 @file:Suppress("OPT_IN_USAGE")
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     var number = 0
+    val times = 10
     runBlocking {
-        val times = 10_000
-        val worker1 = launch(Dispatchers.IO.limitedParallelism(1)) {
-            repeat(times) { number++ }
+        launch(Dispatchers.IO.limitedParallelism(1)) {
+            repeat(times) { number++; println("++: $number") }
+            //yield()
         }
-        val worker2 = launch(Dispatchers.IO.limitedParallelism(1)) {
-            repeat(times) { number-- }
+        launch(Dispatchers.IO.limitedParallelism(1)) {
+            repeat(times) { number--; println("--: $number") }
         }
-        joinAll(worker1, worker2)
     }
-    println(number)
+    println(number) // 0
 }
