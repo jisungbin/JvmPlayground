ce4f9ea83837bd4044236fa0bdc2dadd48e3e4dd
jisungbin <ji@sungb.in>
2023-01-07 06:09:17 +0900

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,14 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-
 fun main() {
-    val stateFlow = MutableStateFlow(0)
-    val flow = stateFlow.asStateFlow()
-    runBlocking {
-        launch {
-            repeat(100) {
-                delay(1000)
-                stateFlow.update { it + 1 }
-            }
-        }
-        launch {
-            flow.collect { value ->
-                println("#1: $value")
-            }
-        }
-        launch {
-            flow.collect { value ->
-                println("#2: $value")
-            }
-        }
-        launch {
-            flow.collect { value ->
-                println("#3: $value")
-            }
-        }
-    }
+    println(
+        """
+        linear-gradient(
+            120deg,
+            #FF1744,
+            #E040FB,
+            #2979FF,
+            #00E5FF,
+            #76FF03
+        )
+        """.trimIndent()
+    )
 }
