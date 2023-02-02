7720e87d2ca41d22c87dd5f00df40ba5c91af793
jisungbin <ji@sungb.in>
2023-02-02 17:57:56 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,5 @@
 fun main() {
-    try {
-        error("")
-    } finally {
-        println("err")
-    }
+    val list = listOf(1, 2, 3, 4, 5)
+    val filter = listOf(1, 2, 3, 4)
+    println(list.dropWhile(filter::contains))
 }
