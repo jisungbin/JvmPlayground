c984adcddc6dfb93dff26a31877711dee0d2ead2
jisungbin <ji@sungb.in>
2023-02-14 02:40:44 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,4 @@
-fun main() {
-    arrayOf(1, 2).forEach(::println)
-    arrayListOf(2, 3).forEach(::println)
-    listOf(3, 4).forEach(::println)
+fun `am_I_awesome?`() {}
+fun test(){
+    ::`am_I_awesome?`
 }
