7e5b31a0409fed75be052c713ba8191416182cf6
jisungbin <ji@sungb.in>
2023-02-23 02:06:25 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,4 @@
-object Modifier
-
-interface A {
-    fun Modifier.a()
-}
-
-interface B {
-    fun Modifier.b()
-}
-
-object AScope : A {
-    override fun Modifier.a() {}
-}
-
-object BScope : B {
-    override fun Modifier.b() {}
-}
-
-object Test : A by AScope, B by BScope
-
-fun A.test() {
-    Modifier.a()
+fun main() {
+    val list = mutableListOf(1)
+    list += 1
 }
