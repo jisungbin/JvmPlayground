f5ceed992b68d793d990cc79b6dc207947eff9a2
jisungbin <ji@sungb.in>
2023-03-03 05:49:00 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("org.slf4j:slf4j-simple:2.0.6")
     implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.4")
+    implementation("com.squareup:kotlinpoet:1.12.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
@@ -64,4 +65,3 @@ dependencies {
     testImplementation("io.strikt:strikt-core:0.34.1")
     testImplementation("com.google.truth:truth:1.1.3")
 }
-
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,44 @@
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.asTypeName
+
+enum class Type {
+    Text,
+}
+
+private val textModifiers = listOf("A", "B")
+
+internal val coreModifiers: Map<Type, List<String>>
+    get() {
+        val map = mutableMapOf<Type, List<String>>()
+        map[Type.Text] = textModifiers
+        return map
+    }
+
 fun main() {
-    val list = mutableListOf(1)
-    list += 1
+    val fileSpec = FileSpec.builder("com.example", "MyClass")
+        .addProperty(
+            PropertySpec.builder(
+                "coreModifiers",
+                Map::class.asTypeName()
+                    .parameterizedBy(
+                        Type::class.asTypeName(),
+                        List::class.asTypeName().parameterizedBy(String::class.asTypeName())
+                    )
+            )
+                .getter(
+                    FunSpec.getterBuilder()
+                        .addStatement("val map = mutableMapOf<Type, List<String>>()")
+                        .addStatement("map[Type.Text] = %L", textModifiers)
+                        .addStatement("return map")
+                        .build()
+                )
+                .build()
+        )
+        .build()
+
+    val code = fileSpec.toString()
+    println(code)
 }
