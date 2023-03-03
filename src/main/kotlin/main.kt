e50f57ba0cbe92d38128bf1712c22930648bd453
jisungbin <ji@sungb.in>
2023-03-03 11:25:00 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,44 +1,8 @@
-import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.PropertySpec
-import com.squareup.kotlinpoet.asTypeName
-
-enum class Type {
-    Text,
-}
-
-private val textModifiers = listOf("A", "B")
-
-internal val coreModifiers: Map<Type, List<String>>
-    get() {
-        val map = mutableMapOf<Type, List<String>>()
-        map[Type.Text] = textModifiers
-        return map
-    }
-
-fun main() {
-    val fileSpec = FileSpec.builder("com.example", "MyClass")
-        .addProperty(
-            PropertySpec.builder(
-                "coreModifiers",
-                Map::class.asTypeName()
-                    .parameterizedBy(
-                        Type::class.asTypeName(),
-                        List::class.asTypeName().parameterizedBy(String::class.asTypeName())
-                    )
-            )
-                .getter(
-                    FunSpec.getterBuilder()
-                        .addStatement("val map = mutableMapOf<Type, List<String>>()")
-                        .addStatement("map[Type.Text] = %L", textModifiers)
-                        .addStatement("return map")
-                        .build()
-                )
-                .build()
-        )
-        .build()
-
-    val code = fileSpec.toString()
-    println(code)
+internal val aideComponents: Map<String, List<String>> = run {
+    val aide = mutableMapOf<String, List<String>>()
+    aide["QuackAnimatedContent"] = listOf("QuackAnimatedContent")
+    aide["QuackAnimatedVisibility"] = listOf("QuackAnimatedVisibility")
+    aide["text"] = listOf("QuackText")
+    aide["theme"] = listOf("QuackTheme")
+    aide
 }
