41361a0b94adff4441f91a3efd62e8c4547779b3
jisungbin <ji@sungb.in>
2023-03-14 07:43:41 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,35 @@
-import androidx.compose.compiler.plugins.kotlin.lower.IrSourcePrinterVisitor
+interface Hi
+
+class HiImpl : Hi
+
+class Test {
+    var test: Int? = 1
+    public fun AAAA(
+        text: String? = "HI~~~",
+        number: Int? = 1_231_111,
+        hi: Hi? = HiImpl(),
+    ) {
+        test = number
+    }
+
+    init {
+        AAAA()
+    }
+}
 
 fun main() {
-    IrSourcePrinterVisitor(System.out)
+    val a = Class.forName("Test").getConstructor().newInstance()
+    val b = a.javaClass
+
+    b.getMethod(
+        "AAAA\$default",
+        b,
+        java.lang.String::class.java,
+        java.lang.Integer::class.java,
+        Hi::class.java,
+        Int::class.java,
+        java.lang.Object::class.java,
+    ).invoke(a, a, null, null, null, Int.MAX_VALUE, null)
+
+    println(b.getMethod("getTest").invoke(a))
 }
