dad0fa574a0bad821987b3955ec6c16dab9eb12f
jisungbin <ji@sungb.in>
2023-03-19 08:23:09 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,35 +1,7 @@
-interface Hi
-
-class HiImpl : Hi
-
-class Test {
-    var test: Int? = 1
-    public fun AAAA(
-        text: String? = "HI~~~",
-        number: Int? = 1_231_111,
-        hi: Hi? = HiImpl(),
-    ) {
-        test = number
-    }
-
-    init {
-        AAAA()
-    }
+enum class A(var a: Int) {
+    HI(1), B(2);
 }
 
 fun main() {
-    val a = Class.forName("Test").getConstructor().newInstance()
-    val b = a.javaClass
-
-    b.getMethod(
-        "AAAA\$default",
-        b,
-        java.lang.String::class.java,
-        java.lang.Integer::class.java,
-        Hi::class.java,
-        Int::class.java,
-        java.lang.Object::class.java,
-    ).invoke(a, a, null, null, null, Int.MAX_VALUE, null)
-
-    println(b.getMethod("getTest").invoke(a))
+    A.HI.a
 }
