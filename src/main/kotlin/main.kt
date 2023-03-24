8e14c55575b5c4e1e34f27755ada003caa1be72e
jisungbin <ji@sungb.in>
2023-03-25 00:34:40 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,1 +1,15 @@
-fun main() {}
+@Suppress("CONFLICTING_PROJECTION", "VARIANCE_ON_TYPE_PARAMETER_NOT_ALLOWED")
+inline fun <reified in T> printNumbers(numbers: List<in Int>): Class<T> {
+    for (number in numbers) {
+        println(number)
+    }
+    return T::class.java
+}
+
+val ints: List<Int> = listOf(1, 2, 3)
+val anys: List<Any> = listOf("hello", 1, 2.0)
+
+fun main() {
+    printNumbers<Int>(ints)
+    printNumbers<Int>(anys)
+}
