9e952b6480b10fd1daf320cf473051c3bfe98b9d
jisungbin <ji@sungb.in>
2023-03-26 06:46:13 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
-fun main() {
-
+interface Modifier {
+    companion object : Modifier
 }
+
+fun Modifier.test() = this
+fun Modifier.test2() = this
+
+fun main() {
+    val modifier = Modifier.test().test2()
+    modifierFun(modifier)
+}
+
+fun modifierFun(modifier: Modifier) {}
