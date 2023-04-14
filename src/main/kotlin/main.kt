7aaf1fd18e39f986460794d2b9aeabdcf3b6eca8
jisungbin <ji@sungb.in>
2023-04-15 04:18:10 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,41 +1,4 @@
-import kotlin.concurrent.thread
+interface Root
+interface Test<A : Root, B : Root>
 
-class StringPrinter {
-    private var value: String? = null
-
-    fun delayedPrint(name: String, delay: Int = 1000) {
-        value = name
-        sleep(delay)
-        println(value)
-    }
-}
-
-class LocalStringPrinter {
-    private val value = ThreadLocal<String>()
-
-    fun delayedPrint(name: String, delay: Int = 1000) {
-        value.set(name)
-        sleep(delay)
-        println(value.get())
-    }
-}
-
-fun main() {
-    val stringPrinter = StringPrinter()
-    thread { stringPrinter.delayedPrint("threadA") }
-    thread { stringPrinter.delayedPrint("threadB") }
-
-    println()
-
-    val localStringPrinter = LocalStringPrinter()
-    thread { localStringPrinter.delayedPrint("localThreadA") }
-    thread { localStringPrinter.delayedPrint("localThreadB") }
-}
-
-private fun sleep(millis: Int) {
-    try {
-        Thread.sleep(millis.toLong())
-    } catch (e: InterruptedException) {
-        e.printStackTrace()
-    }
-}
+fun Test<*, *>.hi() {}
