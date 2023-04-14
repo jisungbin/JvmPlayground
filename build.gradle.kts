e6482099de849082062029494ec4dd5c09d420cd
jisungbin <ji@sungb.in>
2023-04-15 05:34:50 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
+@file:Suppress("VulnerableLibrariesLocal")
+
 import kotlinx.benchmark.gradle.JvmBenchmarkTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.10"
     id("org.jetbrains.kotlinx.benchmark") version ("0.4.7")
+    id("land.sungbin.dependency.handler.extensions") version ("1.1.0-alpha")
     application
 }
 
-group = "land.sungbin.playground"
-version = "1.0.0"
-
 repositories {
     mavenCentral()
+    gradlePluginPortal()
     google()
 }
 
@@ -36,33 +37,42 @@ benchmark {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
+        jvmTarget = "17"
         freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
     }
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_17.toString()
+    targetCompatibility = JavaVersion.VERSION_17.toString()
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+@Suppress("INLINE_FROM_HIGHER_PLATFORM")
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
-    implementation("io.ktor:ktor-server-core:2.2.1")
-    @Suppress("VulnerableLibrariesLocal")
-    implementation("io.ktor:ktor-server-netty:2.2.1")
-    implementation("commons-io:commons-io:2.11.0")
-    implementation("org.slf4j:slf4j-api:2.0.6")
-    implementation("org.slf4j:slf4j-simple:2.0.6")
-    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.6")
-    implementation("com.squareup:kotlinpoet:1.12.0")
-    implementation("org.jetbrains.dokka:dokka-base:1.8.10")
-    implementation("org.jetbrains.dokka:dokka-core:1.8.10")
-    implementation("org.jetbrains.kotlin:kotlin-compiler:1.8.10")
-    implementation("org.jetbrains.compose.compiler:compiler-hosted:1.4.2")
-
+    implementations(
+        "org.jetbrains.kotlin:kotlin-reflect:1.7.20",
+        "org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.6",
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4",
+        "org.jetbrains.dokka:dokka-base:1.8.10",
+        "org.jetbrains.dokka:dokka-core:1.8.10",
+        "org.jetbrains.kotlin:kotlin-compiler:1.8.10",
+        "org.jetbrains.compose.compiler:compiler-hosted:1.4.2",
+        "io.ktor:ktor-server-core:2.2.1",
+        "io.ktor:ktor-server-netty:2.2.1",
+        "commons-io:commons-io:2.11.0",
+        "com.squareup:kotlinpoet:1.12.0",
+        "org.slf4j:slf4j-api:2.0.6",
+        "org.slf4j:slf4j-simple:2.0.6",
+    )
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.20")
-    testImplementation("io.strikt:strikt-core:0.34.1")
-    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementations(
+        "org.junit.jupiter:junit-jupiter-api:5.9.2",
+        "org.jetbrains.kotlin:kotlin-test:1.7.20",
+        "io.strikt:strikt-core:0.34.1",
+        "com.google.truth:truth:1.1.3",
+    )
 }
--- src/main/kotlin/main.kt
+++
@@ -1,4 +0,0 @@
-interface Root
-interface Test<A : Root, B : Root>
-
-fun Test<*, *>.hi() {}
