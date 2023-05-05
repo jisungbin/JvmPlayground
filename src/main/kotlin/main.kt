69800707fd49f3f13cd6837d01720ddabaee4f59
jisungbin <ji@sungb.in>
2023-05-05 23:39:41 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,8 @@
-@NoCopy
-data class Adult private constructor(
-    val age: Int,
-    val name: String,
-) {
-    companion object {
-        fun from(name: String): Adult {
-            return if (name == "Zohn") {
-                Adult(age = 49, name = "Zohn")
-            } else {
-                error("No one allowed except \"Zohn\".")
-            }
-        }
-    }
+fun test() {
+    println(Thread.currentThread().stackTrace[1].methodName)
+    println(object : Any() {}.javaClass.enclosingMethod.name)
 }
 
 fun main() {
-    val Zosh = Adult.from("Zohn").copy(age = 21, name = "Zosh")
-    println(Zosh)
+    test()
 }
