8b6dc1150c60a2dab70a3ab6e6b27b50c3c6ed85
jisungbin <ji@sungb.in>
2023-05-23 05:55:43 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,9 @@
-fun test(hi: String.() -> Int, bye: Unit.(hi: Int) -> String) {
-    "".hi()
-    bye(Unit, 1)
-}
-
-fun rawTest(hi: Function1<String, Int>, bye: Function2<Unit, Int, String>) {
-    test(hi = hi, bye = bye)
-}
-
-fun rawTest2(hi: (String) -> Int, bye: Function2<Unit, Int, String>) {
-    test(hi = hi, bye = bye)
-}
-
-fun QuackText(
-    function: (unit: Unit, unit2: Unit, unit3: () -> Unit) -> Unit,
-) {
+fun test(value: (() -> Unit) -> Unit) {
+  value {
+    print(1)
+  }
 }
 
 fun main() {
-    rawTest(
-        hi = { v ->
-            println(v)
-            2
-        },
-        bye = { a, b ->
-            println(a)
-            b.toString()
-        }
-    )
+  test { it() }
 }
