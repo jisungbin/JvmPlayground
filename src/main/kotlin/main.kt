d223702f454df2f6b78a8ff5a8ab067213ebb971
jisungbin <ji@sungb.in>
2023-07-03 16:11:52 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import org.jetbrains.dokka.model.doc.B
 
-fun main() = runBlocking {
-    println(Thread.currentThread().name)
-    withContext(Dispatchers.IO) {
-        println(Thread.currentThread().name)
-    }
+interface A {
+    val a: Int
+    var aa: Int
+}
+@Suppress("VAR_OVERRIDDEN_BY_VAL_BY_DELEGATION")
+class C(a: A) : A by a/*, B*/ {
+    override val a = 2
 }
