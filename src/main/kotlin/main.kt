c29ec9aca43ea88a311fb027b450827f0d598e2a
jisungbin <ji@sungb.in>
2023-10-30 12:28:07 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,4 @@
-val list = listOf(3, 1, 4, 2, 5, 6, 2)
-const val targetIndex = 2 // 4
-
 fun main() {
-    list
-        .withIndex()
-        .sortedByDescending { (index, _) -> index == targetIndex }
-        .map(IndexedValue<Int>::value)
-        .let(::println)
+  val list = listOf("b", "c", "a")
+  println(list.sortedBy { it })
 }
