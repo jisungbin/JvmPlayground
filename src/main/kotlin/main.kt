20cc175ca5533e999749b0da7b6a9d5784bb1319
jisungbin <ji@sungb.in>
2024-03-20 05:00:19 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,12 @@
-const val z = 0
-const val a = 1 shl 0
-const val b = 1 shl 1
-const val c = 1 shl 2
-const val d = 1 shl 3
-const val e = 1 shl 4
+interface A
+interface B
+interface C
+interface D
+
+interface R : A, B, C
 
 fun main() {
-  val abc = a or b or c
-
-  println(
-    """
-    $z (${Integer.toBinaryString(z)})
-    $a (${Integer.toBinaryString(a)})
-    $b (${Integer.toBinaryString(b)})
-    $c (${Integer.toBinaryString(c)})
-    $d (${Integer.toBinaryString(d)})
-    $e (${Integer.toBinaryString(e)})
-    
-    $abc (${Integer.toBinaryString(abc)})
-   
-    ${abc and z}
-    ${abc and a}
-    ${abc and b}
-    ${abc and c}
-    ${abc and d}
-    ${abc and e}
-  """.trimIndent()
-  )
+    val r: R = object : R {}
+    val rAsA = r as A
+    println(rAsA::class.java.isAssignableFrom(D::class.java))
 }
