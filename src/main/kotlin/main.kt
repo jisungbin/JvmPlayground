edf1441fb812b18767cf95e4fc5be8c7efa6ae33
jisungbin <ji@sungb.in>
2024-03-27 22:26:13 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,4 @@
-interface A
-interface B
-interface C
-interface D
-
-interface R : A, B, C
-
 fun main() {
-    val r: R = object : R {}
-    val rAsA = r as A
-    println(rAsA::class.java.isAssignableFrom(D::class.java))
+    val a = ""
+    a.get(1)
 }
