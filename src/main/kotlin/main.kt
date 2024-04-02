5ac76b47f98157092d2083d3c9dea1e527d5eebf
jisungbin <ji@sungb.in>
2024-04-02 15:01:46 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,44 @@
-import java.net.URL
+import java.util.concurrent.CompletableFuture
+
+internal class SharedResource {
+  private var state = 0
+
+  fun increase() {
+    state++
+  }
+
+  fun decrease() {
+    state--
+  }
+
+  fun initialize() {
+    state = 0
+  }
+
+  val isCorrupted: Boolean
+    get() = this.state != 0
+}
 
 fun main() {
-  println(URL("http://www.example.com").toString())
+  var corruptedStateCount = 0
+  val sharedResource = SharedResource()
+  for (index in 0..99999) {
+    sharedResource.initialize()
+    val thread1 = CompletableFuture.runAsync {
+      for (subIndex in 0..99) {
+        sharedResource.increase()
+      }
+    }
+    val thread2 = CompletableFuture.runAsync {
+      for (subIndex in 0..99) {
+        sharedResource.decrease()
+      }
+    }
+    thread1.join()
+    thread2.join()
+    if (sharedResource.isCorrupted) {
+      corruptedStateCount++
+    }
+  }
+  println(String.format("Corrupted shared resource count - %s", corruptedStateCount))
 }
