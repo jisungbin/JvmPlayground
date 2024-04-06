1632444fa98abbbc9e87741b784659faf8e150ef
jisungbin <ji@sungb.in>
2024-04-07 06:32:02 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,54 +1,32 @@
 @file:Suppress("NOTHING_TO_INLINE")
 
 @JvmInline
-value class NodeKind(val mask: Int) {
-  inline infix fun or(other: NodeKind): Int = mask or other.mask
+value class ContributionKind(val mask: Int) {
   inline infix fun or(other: Int): Int = mask or other
+  inline infix fun or(other: ContributionKind): ContributionKind = ContributionKind(mask or other.mask)
 }
 
-interface Node {
-  val kindSet: Int
-}
+inline infix fun Int.or(other: ContributionKind): Int = this or other.mask
+inline operator fun Int.contains(value: ContributionKind): Boolean = this and value.mask != 0
+inline operator fun ContributionKind.contains(value: ContributionKind): Boolean = this.mask and value.mask != 0
 
-inline infix fun Int.or(other: NodeKind): Int = this or other.mask
-
-inline operator fun Int.contains(value: NodeKind) = this and value.mask != 0
-
-object Nodes {
-  val MapNodeComparator = Comparator<Node> { a, b -> a.kindSet.compareTo(b.kindSet) }
-
-  inline val Any get() = NodeKind(0b1 shl 0)
-  inline val LayoutNodeProvider get() = NodeKind(0b1 shl 1)
-  inline val LayoutNodeConsumer get() = NodeKind(0b1 shl 2)
-  inline val ContentUpdater get() = NodeKind(0b1 shl 3)
-  inline val ContentReader get() = NodeKind(0b1 shl 4)
+object Contributions {
+  inline val None get() = ContributionKind(0b1 shl 0)
+  inline val NaverMap get() = ContributionKind(0b1 shl 1)
+  inline val MapView get() = ContributionKind(0b1 shl 2)
+  inline val Overlay get() = ContributionKind(0b1 shl 3)
 }
 
 fun main() {
-  val layoutNodeProviderNode = object : Node {
-    override val kindSet: Int = Nodes.LayoutNodeProvider.mask
-    override fun toString(): String = "LayoutNodeProviderNode"
-  }
-  val layoutNodeConsumerNode = object : Node {
-    override val kindSet: Int = Nodes.LayoutNodeConsumer.mask
-    override fun toString(): String = "LayoutNodeConsumerNode"
-  }
-  val layoutNodeConsumerNode2 = object : Node {
-    override val kindSet: Int = Nodes.LayoutNodeConsumer.mask
-    override fun toString(): String = "LayoutNodeConsumerNode2"
-  }
-  val contentUpdateNode = object : Node {
-    override val kindSet: Int = Nodes.ContentUpdater.mask
-    override fun toString(): String = "ContentUpdateNode"
-  }
-  val contentReaderNode = object : Node {
-    override val kindSet: Int = Nodes.ContentReader.mask
-    override fun toString(): String = "ContentReaderNode"
-  }
-  listOf(layoutNodeProviderNode, layoutNodeConsumerNode, layoutNodeConsumerNode2, contentUpdateNode, contentReaderNode)
-    .shuffled()
-    .onEach { println("Shuffled: $it") }
-    .also { println("\n") }
-    .sortedWith(Nodes.MapNodeComparator)
-    .forEach { println("Sorted: $it") }
+  val contributions = Contributions.NaverMap or Contributions.MapView
+  // this.mask and value.mask
+  println("Contributions.NaverMap in contributions: ${(Contributions.NaverMap.mask and Contributions.NaverMap.mask).toString(2)}")
+  println("Contributions.NaverMap in contributions: ${(Contributions.None.mask and contributions.mask).toString(2)}")
+  println("Contributions.NaverMap in contributions: ${Contributions.NaverMap in contributions}")
+  println("contributions in contributions: ${contributions in contributions}")
+  println("contributions: " + contributions.mask.toString(2))
+  println("Contributions.None: " + Contributions.None.mask.toString(2))
+  println("Contributions.NaverMap: " + Contributions.NaverMap.mask.toString(2))
+  println("Contributions.MapView: " + Contributions.MapView.mask.toString(2))
+  println("Contributions.Overlay: " + Contributions.Overlay.mask.toString(2))
 }
