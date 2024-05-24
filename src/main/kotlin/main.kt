d1520a3cc0a8d2c262dd66ddc0451f9532d5fa40
jisungbin <ji@sungb.in>
2024-05-24 15:56:27 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,8 @@
-import okio.FileSystem
-import okio.Path.Companion.toPath
-import okio.buffer
-
 fun main() {
-  FileSystem.SYSTEM
-    .openReadWrite("/Users/jisungbin/IdeaProjects/JvmPlayground/src/main/kotlin/TEST.txt".toPath(), mustExist = true)
-    .use { fs ->
-      val current = fs.source().buffer().use { it.readUtf8LineStrict() }
-      fs.resize(0L)
-      fs.sink().buffer().use { sink ->
-        sink.writeUtf8(current)
-        sink.writeUtf8("\n")
-        sink.writeUtf8("Bye, World!")
-      }
-    }
+  val url = "http://localhost:50025/?a=b&snack=milk-classic"
+  val content = "<-- POST 200 $url (2ms)"
+  val regex = Regex("""<-- POST 200 ${url.regexSafe().also(::println)} \(\d+ms\)""")
+  println(content.matches(regex))
 }
+
+private fun String.regexSafe() = replace(Regex("[.^$|()\\[\\]{}*+?\\\\]")) { "\\${it.value}" }
