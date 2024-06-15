6a0ec69ed79df01d2fa01d789b98a064578a597f
Ji Sungbin <ji@sungb.in>
2024-06-16 06:51:19 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,40 +1,6 @@
-import java.io.File
-
 fun main() {
-  val target = File("/Users/jisungbin/AndroidStudioProjects/ComposeInvestigator")
-  val calculation = {
-    target.walkBottomUp()
-      .filterNot { it.path.contains("build") }
-      .filter { it.path.contains("src") }
+  val string = buildString {
+    append("Hello, World!", 0, 5)
   }
-
-  calculation().forEach { file ->
-    val message = { "Failed to rename file: ${file.path}" }
-    when {
-      file.name.contains("_tracking_") -> {
-        check(file.renameTo(File(file.path.replace("_tracking_", "_tracing_"))), lazyMessage = message)
-      }
-      file.name.contains("Tracker") -> {
-        check(file.renameTo(File(file.path.replace("Tracker", "Tracer"))), lazyMessage = message)
-      }
-      file.name.contains("Tracking") -> {
-        check(file.renameTo(File(file.path.replace("Tracking", "Tracing"))), lazyMessage = message)
-      }
-      file.name.contains("TrackTable") -> {
-        check(file.renameTo(File(file.path.replace("TrackTable", "TraceTable"))), lazyMessage = message)
-      }
-      file.name.contains("TrackLower") -> {
-        check(file.renameTo(File(file.path.replace("TrackLower", "TraceLower"))), lazyMessage = message)
-      }
-      file.name.contains("tracktable") -> {
-        check(file.renameTo(File(file.path.replace("tracktable", "tracetable"))), lazyMessage = message)
-      }
-    }
-  }
-
-  calculation().forEach { file ->
-    if (file.name.contains("track", ignoreCase = true)) {
-      println(file.path)
-    }
-  }
+  println(string)
 }
