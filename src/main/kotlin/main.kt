87c483f9f1c227e52587774199de47f5d50817cd
Ji Sungbin <ji@sungb.in>
2024-06-16 16:06:56 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,22 @@
 import okio.Buffer
 
 fun main() {
-  val buffer = Buffer()
-  buffer.writeUtf8("Hello, World!").writeUtf8("\n")
-  println(buffer[buffer.size - 1])
-  println("\n".single().code)
+  val buffer = Buffer().writeUtf8("hello")
+  println(buffer.snapshot().utf8())
+  buffer.readAndWriteUnsafe().use { cursor ->
+    val previousSize = cursor.resizeBuffer(buffer.size + 4)
+    cursor.seek(0)
+    cursor.data!!.copyInto(
+      destination = cursor.data!!,
+      destinationOffset = 2,
+      startIndex = cursor.start,
+      endIndex = previousSize.toInt(),
+    )
+    cursor.data!![cursor.start] = 'a'.code.toByte()
+    cursor.data!![cursor.start + 1] = 'b'.code.toByte()
+    cursor.seek(cursor.buffer!!.size - 2)
+    cursor.data!![cursor.start] = 'c'.code.toByte()
+    cursor.data!![cursor.start + 1] = 'd'.code.toByte()
+  }
+  println(buffer.readUtf8())
 }
