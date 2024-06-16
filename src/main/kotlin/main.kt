3a118a68065d42ab8125d139bcbf44b83db0f626
Ji Sungbin <ji@sungb.in>
2024-06-16 16:55:39 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,4 @@
 fun main() {
-  val UPPER_CASE_OFFSET = ('A'.code - 'a'.code).toByte()
-  val LOWER_CASE_OFFSET = ('a'.code - 'A'.code).toByte()
-
-  fun Byte.uppercase(): Byte {
-    if (this < 'a'.code.toByte() || this > 'z'.code.toByte()) return this
-    return (this + UPPER_CASE_OFFSET).toByte()
-  }
-
-  fun Byte.lowercase(): Byte {
-    if (this < 'A'.code.toByte() || this > 'Z'.code.toByte()) return this
-    return (this + LOWER_CASE_OFFSET).toByte()
-  }
-
-  for (char in 'a'..'z') {
-    println(char.code.toByte().uppercase().toInt().toChar())
-  }
-
-  for (char in 'A'..'Z') {
-    println(char.code.toByte().lowercase().toInt().toChar())
-  }
+  val numbers = listOf(5, 4, 3, 2, 1)
+  println(numbers.sortedBy { it })
 }
