1b0bed46351b8f2acf1d75e8499a9c1089bb4ca3
Ji Sungbin <ji@sungb.in>
2024-06-17 20:22:48 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,29 @@
+import okio.Buffer
+
 fun main() {
-  val numbers = listOf(5, 4, 3, 2, 1)
-  println(numbers.sortedBy { it })
+  val buffer = Buffer().writeUtf8(('a'..'z').joinToString(separator = ""))
+  buffer.readAndWriteUnsafe().use { cursor ->
+    fun debug() {
+      println("start: ${cursor.start}, offset: ${cursor.offset}, end: ${cursor.end}")
+      println("content: ${cursor.buffer!!.snapshot().utf8()}")
+      println()
+    }
+
+    cursor.seek(0)
+    debug()
+
+    cursor.seek(10)
+    debug()
+
+    cursor.seek(0)
+    debug()
+
+    cursor.seek(20)
+    debug()
+
+    repeat(3) {
+      cursor.buffer!!.writeByte('_'.code)
+    }
+    debug()
+  }
 }
