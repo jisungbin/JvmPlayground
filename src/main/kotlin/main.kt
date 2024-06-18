aa9d7dc2ad912cbd9627be0950b967762553fabc
Ji Sungbin <ji@sungb.in>
2024-06-18 20:29:00 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,36 @@
 import okio.Buffer
 
+private const val UPPER_CASE_OFFSET = ('A'.code - 'a'.code).toByte()
+
 fun main() {
-  val buffer = Buffer().writeUtf8(('a'..'z').joinToString(separator = ""))
-  buffer.readAndWriteUnsafe().use { cursor ->
-    fun debug() {
-      println("start: ${cursor.start}, offset: ${cursor.offset}, end: ${cursor.end}")
-      println("content: ${cursor.buffer!!.snapshot().utf8()}")
-      println()
+  val buffer = Buffer().apply {
+    val chars = ('a'..'z').toList()
+    val charSize = chars.size
+    repeat(8192) {
+      repeat(charSize) { index ->
+        writeByte(chars[index].code)
+      }
     }
-
-    cursor.seek(0)
-    debug()
-
-    cursor.seek(10)
-    debug()
-
-    cursor.seek(0)
-    debug()
-
-    cursor.seek(20)
-    debug()
-
-    repeat(3) {
-      cursor.buffer!!.writeByte('_'.code)
-    }
-    debug()
   }
+  buffer.readAndWriteUnsafe().use { cursor ->
+
+    do {
+      // println("current is ${cursor.data!![cursor.start]} and result is ${cursor.data!![cursor.start].uppercase()}")
+      // cursor.data!![cursor.start] = cursor.data!![cursor.start].uppercase()
+      println(
+        """
+        start: ${cursor.start}
+        end: ${cursor.end}
+        offset: ${cursor.offset}
+        """.trimIndent()
+      )
+      println()
+    } while (cursor.next() != -1)
+  }
+//  println(buffer.readUtf8())
+}
+
+fun Byte.uppercase(): Byte {
+  if (this < 'a'.code.toByte() || this > 'z'.code.toByte()) return this
+  return (this + UPPER_CASE_OFFSET).toByte()
 }
