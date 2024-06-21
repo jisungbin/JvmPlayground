82ce3331a034a042097cd678830ec59e8c30dfec
jisungbin <ji@sungb.in>
2024-06-21 09:57:44 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,12 @@
 fun main() {
-  repeat(10) { println(it) }
-  for (i in 0..10) {
-    println(i)
-  }
+  val a = 0b1 shl 0
+  val b = 0b1 shl 1
+  val c = 0b1 shl 2
+
+  println(a.toString(radix = 2))
+  println(b.toString(radix = 2))
+  println(c.toString(radix = 2))
+  println()
+  println((a or c).toString(radix = 2))
+  println((c or a).toString(radix = 2))
 }
