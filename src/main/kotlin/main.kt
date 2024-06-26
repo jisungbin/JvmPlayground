7253a0d9712cdacd3dc3e1b0be948c54e8bdfb7b
jisungbin <ji@sungb.in>
2024-06-26 10:16:08 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,26 @@
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import okio.buffer
+
 fun main() {
-  println(1.toDouble())
+  FileSystem.SYSTEM
+    .openReadWrite("/Users/jisungbin/IdeaProjects/JvmPlayground/src/main/kotlin/TestFile.txt".toPath())
+    .use { fs ->
+      val source = fs.source().buffer()
+      val sink = fs.sink().buffer()
+
+      while (!source.exhausted()) {
+        val line = source.readUtf8LineStrict()
+        if (line == "Hello, World!") {
+          sink.writeUtf8("Bye, World!")
+          sink.writeUtf8("\n")
+          break
+        }
+        sink.writeUtf8(line)
+        sink.writeUtf8("\n")
+      }
+
+      source.close()
+      sink.close()
+    }
 }
