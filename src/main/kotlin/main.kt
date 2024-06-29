34cbeb7cb2348cb08b5c155cad933e8f5831c6fb
Ji Sungbin <ji@sungb.in>
2024-06-30 04:47:54 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,18 @@
 fun main() {
-  val ints = IntArray(10) { 1 }
-  println(ints[0]++)
-  println(ints[0])
-  println(++ints[0])
+  val container = 0b1 shl 3
+  val zero = 0b1 shl 0
+  val a = 0b1 shl 1
+  val b = 0b1 shl 5
+
+  println(container.toString(radix = 2))
+  println(zero.toString(radix = 2))
+  println(a.toString(radix = 2))
+  println(b.toString(radix = 2))
+
+  println()
+
+  println((a or b).toString(radix = 2))
+  println(((a or b) or container).toString(radix = 2))
+  println((((a or b) or container) or a).toString(radix = 2))
+  println(((((a or b) or container) or a) or zero).toString(radix = 2))
 }
