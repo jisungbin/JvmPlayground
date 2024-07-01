5247f48272e498582102d69bb5c9ce7f07f361de
jisungbin <ji@sungb.in>
2024-07-01 16:58:48 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,22 @@
+private const val NodeBit_Mask = 0b0100_0000_0000_0000__0000_0000_0000_0000
+private const val ObjectKey_Mask = 0b0010_0000_0000_0000__0000_0000_0000_0000
+private const val ObjectKey_Shift = 29
+private const val Aux_Mask = 0b0001_0000_0000_0000__0000_0000_0000_0000
+private const val Aux_Shift = 28
+private const val Mark_Mask = 0b0000_1000_0000_0000__0000_0000_0000_0000
+private const val ContainsMark_Mask = 0b0000_0100_0000_0000__0000_0000_0000_0000
+private const val Slots_Shift = Aux_Shift
+private const val NodeCount_Mask = 0b0000_0011_1111_1111__1111_1111_1111_1111
+
 fun main() {
-  val container = 0b1 shl 3
-  val zero = 0b1 shl 0
-  val a = 0b1 shl 1
-  val b = 0b1 shl 5
-
-  println(container.toString(radix = 2))
-  println(zero.toString(radix = 2))
-  println(a.toString(radix = 2))
-  println(b.toString(radix = 2))
-
-  println()
-
-  println((a or b).toString(radix = 2))
-  println(((a or b) or container).toString(radix = 2))
-  println((((a or b) or container) or a).toString(radix = 2))
-  println(((((a or b) or container) or a) or zero).toString(radix = 2))
+  println(NodeBit_Mask)
+  println(ObjectKey_Mask)
+  println(ObjectKey_Shift)
+  println(Aux_Mask)
+  println(Aux_Shift)
+  println(Mark_Mask)
+  println(ContainsMark_Mask)
+  println(Slots_Shift)
+  println(NodeCount_Mask)
+  println(Int.MAX_VALUE)
 }
