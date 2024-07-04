3c4c16ff0775b3c774a5a27163b74500382210cf
jisungbin <ji@sungb.in>
2024-07-04 12:49:51 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,14 @@
-fun main() {
-  println(Long.MAX_VALUE.toString(radix = 2))
-  println((Int.MAX_VALUE.toLong() shl 32).toString(radix = 2))
-  println(1234.toLong().toString(radix = 2).padStart(63))
-  println(1234.toLong().shl(32).toString(radix = 2).padStart(63))
-  println(0xFFFFFFFF.toString(radix = 2).padStart(63))
-  println(5678.toLong().toString(radix = 2).padStart(63))
-  println((5678.toLong() and 0xFFFFFFFF).toString(radix = 2).padStart(63))
-  println((1234.toLong().shl(32) or (5678.toLong() and 0xFFFFFFFF)).toString(radix = 2).padStart(63))
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+private val flow = MutableSharedFlow<Unit>(replay = 1)
+
+fun main(): Unit = runBlocking {
+  launch { combine(flow, flowOf(Unit)) { _, _ -> }.collect(::println) }
+  makeRequest().getOrNull()?.let { flow.emit(it) }
 }
+
+private fun makeRequest() = Result.success(Unit)
