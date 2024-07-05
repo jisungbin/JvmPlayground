563d5d267423a4bf4839e1b2514ded1082d3ffef
jisungbin <ji@sungb.in>
2024-07-05 10:12:00 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,5 @@
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-
-private val flow = MutableSharedFlow<Unit>(replay = 1)
-
-fun main(): Unit = runBlocking {
-  launch { combine(flow, flowOf(Unit)) { _, _ -> }.collect(::println) }
-  makeRequest().getOrNull()?.let { flow.emit(it) }
+fun main() {
+  print("a")
+  println(10.toChar())
+  print("a")
 }
-
-private fun makeRequest() = Result.success(Unit)
