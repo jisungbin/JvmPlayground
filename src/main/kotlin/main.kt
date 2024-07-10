f682a76cd67f6a9388f2050734ff43c96566c011
jisungbin <ji@sungb.in>
2024-07-10 10:56:09 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,4 @@
-@file:Suppress("UNUSED_VARIABLE")
-
-import org.intellij.lang.annotations.Language
-
 fun main() {
-  // language=kotlin
-  val kotlinCode = """
-    fun main() {
-      println("Hello~")
-    }
-  """.trimIndent()
-
-  @Language("kotlin")
-  val kotlinCode2 = """
-    fun main() {
-      println("Hello~2")
-    }
-  """.trimIndent()
+  println("AAAA".split(' '))
 }
 
