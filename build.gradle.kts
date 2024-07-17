9e972b7ad1d7450727256afd97309b2d35feeaa6
jisungbin <ji@sungb.in>
2024-07-17 10:16:24 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,8 @@ repositories {
 
 dependencies {
   implementation(kotlin("reflect"))
+  implementation(kotlin("stdlib"))
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
   implementation("com.squareup.okio:okio:3.9.0")
-  implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
-  implementation("com.squareup.okhttp3:okhttp-coroutines:5.0.0-alpha.14")
-  implementation("com.squareup.moshi:moshi:1.15.1")
+  implementation("org.jsoup:jsoup:1.18.1")
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,10 @@
-fun main() {
-  println("AAAA".split(' '))
-}
+import kotlin.math.pow
 
+fun main() {
+  println(Integer.toBinaryString(1).padStart(32, '0'))
+  println(Integer.toBinaryString(-1).padStart(32, '0'))
+  println(1.toString(radix = 2))
+  println((-1).toString(radix = 2))
+  println(2.0.pow(32).toUInt().toString(radix = 2).padStart(32, '0'))
+  println(UInt.MAX_VALUE.toString(radix = 2).padStart(32, '0'))
+}
