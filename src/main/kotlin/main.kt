e41ed873c95ff478a3cf8e3ea910efc02707d8f9
Ji Sungbin <ji@sungb.in>
2024-07-28 08:19:54 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,22 @@
 import com.caoccao.javet.interop.V8Host
 import com.caoccao.javet.interop.V8Runtime
+import com.caoccao.javet.interop.converters.JavetProxyConverter
+import com.caoccao.javet.values.reference.V8ValueFunction
 
 fun main() {
   val code = """
-    |let num = 0
-    |
-    |function test() {
-    |  return ++num
+    |function test(returner) {
+    |  return returner.process(10);
     |}
   """.trimMargin()
 
   val runtime = V8Host.getV8Instance().createV8Runtime<V8Runtime>()
+  runtime.converter = JavetProxyConverter()
 
-  runtime.getExecutor(code).executeVoid()
-  println(runtime.getExecutor("test()").executeString())
-  println(runtime.getExecutor("test()").executeString())
-  println(runtime.getExecutor("test()").executeString())
+  val param = runtime.createV8ValueObject().also { obj ->
+    obj.bind(Returner())
+  }
+
+  runtime.getExecutor(code).compileV8Script().executeVoid()
+  println(runtime.getExecutor("test").execute<V8ValueFunction>().callInteger(runtime.globalObject, param))
 }
