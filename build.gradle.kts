e7473eecfde8b03abc1285f566109457becb0154
Ji Sungbin <forky.ji@healingpaper.com>
2024-09-13 10:00:05 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 
 dependencies {
   implementation(kotlin("reflect"))
+  implementation("com.google.code.gson:gson:2.9.0")
 //  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
 //  implementation("com.caoccao.javet:javet-macos:3.1.4")
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
+import java.math.BigDecimal
+import java.text.NumberFormat
+
+private val formatter = NumberFormat.getInstance()
+
+private fun BigDecimal.numberFormat(): String {
+  val number = toLong()
+  val floatingNumber = toString().substringAfterLast('.', missingDelimiterValue = "")
+  return formatter.format(number) + if (floatingNumber.isNotEmpty()) ".$floatingNumber" else ""
+}
+
 fun main() {
-  19.also { println(it.toString() + Unit.toString()) }
+  val number = BigDecimal("123456789.123456789000000")
+  println(number.numberFormat())
 }
