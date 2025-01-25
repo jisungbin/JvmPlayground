aa5a275d0a2777f80248874683bfa894c7408f66
Ji Sungbin <ji@sungb.in>
2025-01-26 01:01:36 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,4 @@
-import kotlin.time.measureTime
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-
 fun main() {
-  measureTime {
-    runBlocking {
-      CoroutineScope(Dispatchers.Default.limitedParallelism(1)).launch {
-        repeat(5) {
-          launch {
-            println(Thread.currentThread().name)
-            Thread.sleep(1000)
-          }
-        }
-      }
-        .join()
-    }
-  }
-    .also { println("Time: $it") }
+  println(Integer.toBinaryString(-10000))
+  println(java.lang.Long.toBinaryString(-10000))
 }
