368d6967552b7bba468f09238ff29d14d0efed5d
Ji Sungbin <ji@sungb.in>
2025-04-28 19:11:30 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,3 @@
-
-import com.squareup.wire.schema.Location
-import com.squareup.wire.schema.SchemaLoader
-import okio.FileSystem
-
 fun main() {
-  val root = Location.get("scheme")
-  val loader = SchemaLoader(FileSystem.SYSTEM)
-  loader.initRoots(listOf(root), listOf(root))
-
-  val widget = loader.load("widget/v1/widget.proto")
-  println(widget)
+  
 }
