2db982ab28bc9b86921e8aefbc19aa726bc407bb
Ji Sungbin <ji@sungb.in>
2025-06-01 19:05:13 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+
 fun main() {
-  
+  val job = Job()
+  repeat(10) { count ->
+    job.invokeOnCompletion { println(it) }
+  }
+  val scope = CoroutineScope(job)
+  scope.cancel()
 }
---
+++ src/main/kotlin/newfolder/MyKotlinFile.kt
@@ -0,0 +1,4 @@
+package newfolder
+
+class MyKotlinFile {
+}
