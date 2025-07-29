cb1b560e574d34fe4a9426e9abf9e23497df0c0c
Ji Sungbin <ji@sungb.in>
2025-07-29 15:56:49 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,3 @@
-abstract class Logger {
-  abstract fun log(message: String)
-}
-
-class ConsoleLogger : Logger() {
-  override fun log(message: String) = println(message)
-}
-
-context(logger: Logger) fun Int.doSomething() {
-  logger.log("hello")
-  // do something
-  logger.log("bye")
-  logger.log(": $this")
-}
-
 fun main() {
-  with(1) {
-    context(ConsoleLogger()) {
-      doSomething()
-    }
-  }
-} 
+  println(Any::class.simpleName)
+}
