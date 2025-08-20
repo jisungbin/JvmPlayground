4e59129d7a3c623b23fcbdc24039c8794ad7969c
Ji Sungbin <ji@sungb.in>
2025-08-21 07:16:50 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,17 @@
-@Suppress("INVALID_CHARACTERS")
-fun `<anonymous>`() {
-  println(123)
-}
+private const val changedLowBitMask = 0b001_001_001_001_001_001_001_001_001_001_0
+private const val changedHighBitMask = changedLowBitMask shl 1
+private const val changedMask = (changedLowBitMask or changedHighBitMask).inv()
 
 fun main() {
-  `<anonymous>`()
+  println(changedLowBitMask.toString(2).padStart(31, '0'))
+  println(changedHighBitMask.toString(2).padStart(31, '0'))
+  println((changedLowBitMask or changedHighBitMask).toString(2).padStart(31, '0'))
+  println(changedMask.toString(2).padStart(31, '0'))
+
+  println("--------------------")
+
+  val num = 0b0110110110110110110110110110110  // 31비트 수
+  val result = num.inv()
+  println(result.toString(2))  // -110110110110110110110110110111 (음수로 출력)
+  println(Integer.toBinaryString(result))  // 비트 패턴 확인
 }
