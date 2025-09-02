b732db7a2e4f08703e542ed39a819ee668318ce9
jisungbin <ji@sungb.in>
2025-09-02 09:40:26 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,5 @@
+import kotlin.math.floor
+
 fun main() {
-  val n = UInt.MAX_VALUE.toInt() and 0b1.inv()
-  println(0b1.inv())
-  println(Integer.toBinaryString(n).padStart(32, '_'))
-  println(Integer.toBinaryString(n ushr 1).padStart(32, '_'))
+  println(floor(7.8))
 }
