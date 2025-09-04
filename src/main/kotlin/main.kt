f22e6315cf9bfa96c8542caf84599f1cbfa5cf16
jisungbin <ji@sungb.in>
2025-09-04 19:11:21 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,20 @@
-import kotlin.math.floor
-
 fun main() {
-  println(floor(7.8))
+  val start = 30
+  val end = 64
+
+  println(
+    java.lang.Long.toBinaryString(
+      (-1L ushr (
+        // 실제로 설정해야 하는 비트까지의 시프팅 거리 계산
+        64 -
+          // 실제로 설정해야 하는 비트 수 계산
+          (end - start)
+        )
+        ) shl start
+    )
+      .reversed()
+      .chunked(10)
+      .reversed()
+      .joinToString("_")
+  )
 }
