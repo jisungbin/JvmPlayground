baca4f26ff054ecd18833ded0c59f6067ec35c76
jisungbin <ji@sungb.in>
2025-09-04 19:17:30 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,10 +8,27 @@ fun main() {
         // 실제로 설정해야 하는 비트까지의 시프팅 거리 계산
         64 -
           // 실제로 설정해야 하는 비트 수 계산
+          (end - start) // 34
+        // 30
+        )
+        )
+    )
+      .padStart(64, '0')
+      .chunked(10)
+      .joinToString("_"),
+  )
+
+  println(
+    java.lang.Long.toBinaryString(
+      (-1L ushr (
+        // 실제로 설정해야 하는 비트까지의 시프팅 거리 계산
+        64 -
+          // 실제로 설정해야 하는 비트 수 계산
           (end - start)
         )
         ) shl start
     )
+      .padStart(64, '0')
       .reversed()
       .chunked(10)
       .reversed()
