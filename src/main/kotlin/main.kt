0e39073d62784a040d9ada780b6ec809804524e4
jisungbin <ji@sungb.in>
2025-09-10 16:55:29 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,5 @@
-private const val changedLowBitMask = 0b001_001_001_001_001_001_001_001_001_001_0
-
-// 0b010_010_010_010_010_010_010_010_010_010_0
-private const val changedHighBitMask = changedLowBitMask shl 1
-
-private const val changedMask = (changedLowBitMask or changedHighBitMask).inv()
+class A(a: Int, b: Int = 2)
 
 fun main() {
-  println(Integer.toBinaryString(changedMask))
+  1.let(::A)
 }
