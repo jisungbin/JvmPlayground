c38058d294cff0c9b03509a506afe35ca3f2e0b8
Ji Sungbin <ji@sungb.in>
2025-09-25 20:01:10 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,36 +1,8 @@
-import com.squareup.wire.AnyMessage
-import com.squareup.wire.Message
-import com.squareup.wire.schema.EmptyWireLogger
-import com.squareup.wire.schema.KotlinTarget
-import com.squareup.wire.schema.Location
-import com.squareup.wire.schema.WireRun
-import component.RepeatingMessage
-import component.TextComponent2
-import okio.FileSystem
-
-fun main() {
-//  wireRun()
-
-  val messages = RepeatingMessage(
-    listOf(
-      AnyMessage.pack(Message),
-      AnyMessage.pack(TextComponent2("2")),
-      AnyMessage.pack(TextComponent("3")),
-    )
-  )
-
-  val bytes = RepeatingMessage.ADAPTER.encode(messages)
-  val decoded = RepeatingMessage.ADAPTER.decode(bytes)
-
-  decoded.messages.forEach { println(it.typeUrl) }
+class A {
+  companion object
 }
 
-fun wireRun() {
-  WireRun(
-    sourcePath = listOf(Location.get("/Users/jisungbin/IdeaProjects/JvmPlayground/src/proto")),
-    targets = listOf(
-      KotlinTarget(outDirectory = "/Users/jisungbin/IdeaProjects/JvmPlayground/src/main/kotlin/proto", escapeKotlinKeywords = true),
-    ),
-  )
-    .execute(FileSystem.SYSTEM, EmptyWireLogger())
+fun main() {
+  println(A == A)
+  println(A === A)
 }
