81e765855d25cec64bc02803f15c38129b666bc0
Ji Sungbin <ji@sungb.in>
2025-09-30 12:59:35 +0900

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,10 @@
-class A {
-  companion object
+import kotlin.reflect.full.functions
+
+class A(private val a: Int) {
+  fun p(b: Any) = a.toString() + b
 }
 
 fun main() {
-  println(A == A)
-  println(A === A)
+  val a = A(1)
+  println(a::class.functions.first { it.name == "p" }.call(a, 2))
 }
